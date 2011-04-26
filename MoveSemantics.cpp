13c8897d71b6b7dc0e1791d1d5feffdac6780be6
Jonathan B Coe <jon@jbcoe.net>
2011-04-26 22:50:55 +0100

    Making progress

--- MoveSemantics.cpp
+++ MoveSemantics.cpp
@@ -3,28 +3,42 @@
 class Moveable
 {
 	public:
+
+		int m_iField;
+	
 		Moveable()
 		{
+			m_iField = 0;
 			std::cout << "Default constructor" << std::endl;
 		}
 
 		Moveable(const Moveable& m)
 		{
+			m_iField = 1;
 			std::cout << "Copy constructor" << std::endl;
 		}
 
 		Moveable(const Moveable&& m)
 		{
+			m_iField = 2;
 			std::cout << "Move constructor" << std::endl;
 		}
 
-		~Moveable() 
+    Moveable& operator = (const Moveable& m)
 		{
-			std::cout << "Destructor" << std::endl;
+			m_iField = m.m_iField;
+			std::cout << "normal assignement" << std::endl;
+		}
+
+    Moveable& operator = (const Moveable&& m)
+		{
+			m_iField = m.m_iField;
+			std::cout << "rvalue assignement" << std::endl;
 		}
 
 		static Moveable Instance()
 		{
+			std::cout << "Instance function" << std::endl;
 			return Moveable();
 		}
 };
@@ -33,5 +47,6 @@ int main()
 {
 	Moveable m;
 	Moveable c(m);
-  Moveable m3(Moveable::Instance());
+  Moveable m3;
+	m3 = c;
 }
