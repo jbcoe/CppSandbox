03cdcd00dca52315457d175bb703814365e95e6e
Jonathan B Coe <jon@jbcoe.net>
2011-04-27 02:06:41 +0100

    Removed pointless member variable.

--- MoveSemantics.cpp
+++ MoveSemantics.cpp
@@ -4,36 +4,29 @@ class Moveable
 {
 	public:
 
-		int m_iField;
-	
 		Moveable()
 		{
-			m_iField = 0;
 			std::cout << "Default constructor" << std::endl;
 		}
 
 		Moveable(const Moveable& m)
 		{
-			m_iField = 1;
 			std::cout << "Copy constructor" << std::endl;
 		}
 
 		Moveable(const Moveable&& m)
 		{
-			m_iField = 2;
 			std::cout << "Move constructor" << std::endl;
 		}
 
     Moveable& operator = (const Moveable& m)
 		{
-			m_iField = m.m_iField;
 			std::cout << "Normal assignement" << std::endl;
 			return *this;
 		}
 
     Moveable& operator = (const Moveable&& m)
 		{
-			m_iField = m.m_iField;
 			std::cout << "rvalue assignement" << std::endl;
     	return *this;
 		}
@@ -47,14 +40,11 @@ class Moveable
 
 int main()
 {
-  ///
 	
 	Moveable m;
 	
 	std::cout << std::endl;
 	
-	///
-
 	Moveable c(m);
 	
 	std::cout << std::endl;
