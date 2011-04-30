b1e333c5ddcfa89109442526439dc13ac9a52600
Jonathan B Coe <jon@jbcoe.net>
2011-04-30 15:03:52 +0100

    Added InitializerLists project. Compiles and runs

---
+++ InitializerLists/InitializerLists.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class SimpleVector
+{
+	public:
+		SimpleVector(std::initializer_list<int> list)
+		{
+			std::cout << "Initialised with " << 
+				list.size() << " variables" << std::endl;
+    	for ( const int& v : list )
+			{
+				std::cout << "  value: " << v << std::endl;
+			}
+		}
+};
+
+int main(int argc, char* argv[])
+{
+  SimpleVector myVector = {1,2,3,4,5,6,7};
+}
