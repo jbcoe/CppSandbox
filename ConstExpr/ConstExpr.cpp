434bfa911c63c2b092397c99b830db87803be8f2
Jonathan B Coe <jon@jbcoe.net>
2011-04-30 14:41:08 +0100

    Added ConstExpr directory.
    CostExpr binary produces no output but compiles.

---
+++ ConstExpr/ConstExpr.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+constexpr double GetG ()
+{
+	return 9.81;
+}
+
+constexpr int GetMyAge()
+{
+	return 32;
+}
+
+int main(int argc, char* argv[])
+{
+	double myValues[GetMyAge()];
+
+	static_assert ( GetG() == 9.81, "Get G does not return 9.81");
+}
