24289d141cf3f4ddf28e15ea4a54c94501551b8f
Jonathan B Coe <jon@jbcoe.net>
2011-04-30 20:20:11 +0100

    Delegating construction does not compile

---
+++ ConstructionImprovement/ConstructionImprovement.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class MyClass
+{
+	int m_iID;
+
+public:
+	MyClass(int iID) : m_iID(iID) 
+	{ 
+		std::cout << "Built and instance with ID: " << m_iID << std::endl; 
+	}
+
+	MyClass() : MyClass(42) 
+	{
+		std::cout << "Delegated construction" << m_iID << std::endl; 
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	MyClass myInstance;
+}
+
