60af49be0a977f0163fd594d6f6f877a31a08424
Jonathan B Coe <jon@jbcoe.net>
2011-04-30 18:30:27 +0100

    Random numbers example compiles and runs

--- RandomNumbers/RandomNumbers.cpp
+++ RandomNumbers/RandomNumbers.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <random>
+#include <functional>
 
 int main(int argc, char* argv[])
 {
+	std::uniform_int_distribution<int> distribution(0,99);
+	std::mt19937 engine;
+	auto generator = std::bind(distribution,engine);
+
+	for ( int i=0; i<50; ++i )
+	{
+		int random = generator();
+		std::cout << random << " ";
+	}
+	std::cout << std::endl;
 }
 
