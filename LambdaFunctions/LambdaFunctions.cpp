0610bbe8220d13298b7e1f9f800c856746269716
Jonathan B Coe <jon@jbcoe.net>
2011-04-30 19:50:41 +0100

    Lambda functions work very nicely

---
+++ LambdaFunctions/LambdaFunctions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(int argc, char* argv[])
+{
+	std::vector<int> some_list = {1,2,3,4,5,6,7,8,9};
+	int count = 0;
+	int sum = 0;
+	std::for_each(some_list.begin(), some_list.end(), 
+			[&count,&sum](int x) 
+			{sum += x; ++count;}
+			);
+
+	std::cout << "Sum: " << sum << " count: "<< count << std::endl;
+}
+
