98b38d8fe42ee9798937a9f6b9f57c86c78004ba
Jonathan B Coe <jon@jbcoe.net>
2011-04-30 18:14:32 +0100

    git integration for new project script

--- newProject.sh
+++ newProject.sh
@@ -5,16 +5,21 @@ PROJECT_NAME=$1
 if [ -e ${PROJECT_NAME} ] 
 then 
 	echo "A file called "${PROJECT_NAME}" exists"
-#	exit -1
+	exit -1
 fi
 
 mkdir $PROJECT_NAME
 
 touch ${PROJECT_NAME}/${PROJECT_NAME}.cpp
 
-#git add ${PROJECT_NAME}/Makefile
-#git add ${PROJECT_NAME}/${PROJECT_NAME}.cpp
-
 cat .MakefileTemplate | sed s/PROJECT_NAME/${PROJECT_NAME}/ > ${PROJECT_NAME}/Makefile
+cat .CPP_Template > ${PROJECT_NAME}/${PROJECT_NAME}.cpp
+
+echo ${PROJECT_NAME}/${PROJECT_NAME} >> .gitignore
+
+git add .gitignore
+git add ${PROJECT_NAME}/Makefile
+git add ${PROJECT_NAME}/${PROJECT_NAME}.cpp
+
 
 echo Created project ${PROJECT_NAME}
