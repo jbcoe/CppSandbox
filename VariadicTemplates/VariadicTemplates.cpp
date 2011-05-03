78be18cc2e3c93ffd55d1de7c36defbd3ab493e3
Jonathan B Coe <jon@jbcoe.net>
2011-05-03 23:58:37 +0100

    Improved indenting

--- VariadicTemplates/VariadicTemplates.cpp
+++ VariadicTemplates/VariadicTemplates.cpp
@@ -20,6 +20,8 @@ int countTemplateArguments(Values...)
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Template function was provided with " << countTemplateArguments(1,2,3,4,5,6,7,8,9,10) << " arguments."<< std::endl;
+	std::cout << "Template function was provided with " 
+						<< countTemplateArguments(1,2,3,4,5,6,7,8,9,10) 
+						<< " arguments."<< std::endl;
 }
 
