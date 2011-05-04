106470331a3d4775fa129a5e8a86185d07b49f28
Jonathan B Coe <jon@jbcoe.net>
2011-05-04 03:33:24 +0100

    Strongly typed enums compile. No output produced

---
+++ EnumClass/EnumClass.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+enum class Day
+{
+	Monday,
+	Tuesday,
+	Wednesday,
+	Thursday,
+	Friday,
+	Saturday,
+	Sunday
+};
+
+enum class Month
+{
+	January,
+	February,
+	March,
+	April,
+	May,
+	June,
+	July,
+	August,
+	September,
+	October,
+	November,
+	December
+};
+
+int main(int argc, char* argv[])
+{
+	Month eMonth;
+	Day eDay;
+
+	//No compilation as types are different
+	//if ( eMonth == eDay ) {}
+
+	eMonth = Month::March;
+	eDay = Day::Wednesday;
+}
+
