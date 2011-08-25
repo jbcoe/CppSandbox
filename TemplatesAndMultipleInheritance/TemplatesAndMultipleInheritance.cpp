cfc46ed9d2f60345da3099b5a776ddaac26d418d
Jonathan B Coe <jon@jbcoe.net>
2011-08-25 09:45:58 +0100

    Added code from another small project

---
+++ TemplatesAndMultipleInheritance/TemplatesAndMultipleInheritance.cpp
@@ -0,0 +1,37 @@
+// Test.cpp
+
+#include <iostream>
+
+////////////////////////////////////////////
+
+template<typename T, typename TInterface>
+struct TSetImpl : public TInterface
+{
+	virtual void doThing(){std::cout << "CSetImpl" << std::endl;}
+};
+
+////////////////////////////////////////////
+
+struct IPublic 
+{
+	virtual void doThing() = 0;
+};
+
+////////////////////////////////////////////
+
+struct CPublicImpl : public TSetImpl<int,IPublic>
+{
+};
+
+////////////////////////////////////////////
+
+int main() 
+{
+	CPublicImpl p;
+	p.doThing();
+	return 0;
+}
+
+////////////////////////////////////////////
+
+// ~Test.cpp
