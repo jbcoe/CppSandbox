43499282fa6986d4b131b5f295ce9dd811ce5a6d
Jonathan B Coe <jon@jbcoe.net>
2011-09-12 15:52:17 +0100

    Used boost bind to add templated function

--- BoostSignals/BoostSignals.cpp
+++ BoostSignals/BoostSignals.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <boost/bind.hpp>
+#include <boost/bind/placeholders.hpp>
 #include <boost/signal.hpp>
 #include <vector>
 #include <numeric>
@@ -24,8 +26,10 @@ void print_all( T first, T last)
 template <typename T>
 void print_sum( T first, T last)
 {
-	std::cout << std::accumulate(first,last,0);
+	std::cout << std::accumulate(first,last,0) << std::endl;
 }
+
+typedef std::vector<double>::const_iterator DVec_it;
 
 int main(int argc, char* argv[])
 {
@@ -36,17 +40,21 @@ int main(int argc, char* argv[])
 	signal();
 
 	boost::signal2<void,
-			std::vector<double>::const_iterator,
-			std::vector<double>::const_iterator > listSignal;
+			DVec_it,
+			DVec_it > listSignal;
 
 	std::vector<double> myList = {1.0, 2.0, 3.0};
 
-  typedef std::vector<double>::const_iterator DVec_it;
-
-	listSignal.connect(&print_all<std::vector<double>::const_iterator>);
+	listSignal.connect(&print_all<DVec_it>);
 	
-	listSignal.connect(&print_sum<std::vector<double>::const_iterator>); 
-  
+	listSignal.connect(&print_sum<DVec_it>); 
+
+	listSignal.connect( boost::bind(
+			std::accumulate<DVec_it, double>, 
+			_1, 
+			_2, 
+			0.0));
+
 	listSignal(myList.begin(),myList.end());
 }
 
