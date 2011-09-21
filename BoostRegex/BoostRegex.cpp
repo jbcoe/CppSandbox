14de4ebb383ece6281b2e0683b1ca312124e1a7b
Jonathan B Coe <jon@jbcoe.net>
2011-09-21 23:47:54 +0100

    Bad character parser works nicely

---
+++ BoostRegex/BoostRegex.cpp
@@ -0,0 +1,48 @@
+#include <boost/regex.hpp>
+#include <iostream>
+#include <sstream>
+
+const char* illegalCharacters =
+	"(\\[)|(\\])|"
+	"(<)|(>)|"
+	"(\\*)|"
+	"(&)|"
+	"(\\t)|"
+	"(\\^)|"
+	"(%)|"
+	"(#)|"
+	"(£)|"
+	"(@)|"
+	"(!)|"
+	"(:)|"
+	"(;)|"
+	"(\\\")|"
+	"(')|"
+	"(\\$)|"
+	"(/)|"
+	"(\\))|"
+	"(\\()|"
+	"(\\\\)|"
+	"( )";
+
+int main(int argc, char* argv[])
+{
+	std::string badFilename = "[]<>*&^%$£@!#()\"':;\\/ \t";
+	std::cout << "FINAL   __" << badFilename << "__"<< std::endl;
+
+	boost::regex exp(illegalCharacters);
+
+	std::stringstream ss;
+
+	std::ostream_iterator<char,char> oi(ss);
+
+	boost::regex_replace(
+			oi, badFilename.begin(), badFilename.end(), 
+			exp, "X",
+			boost::match_default | boost::format_all );
+
+	std::string corrected(ss.str());
+	
+	std::cout << "INITIAL __" << corrected << "__"<< std::endl;
+}
+
