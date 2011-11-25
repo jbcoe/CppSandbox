9dcd49238d3c077af737845e574b1bdaccbe31cc
Jonathan Coe <jon@jbcoe.net>
2011-11-25 20:31:57 +0000

    Variadic argument list for member function template

--- FunctionPointers/FunctionPointers.cpp
+++ FunctionPointers/FunctionPointers.cpp
@@ -1,12 +1,13 @@
+#include <utility>
 #include <iostream>
 
 // A rather pointless function to illustrate a pointer to member function
 
-template <typename Return_t, typename Class_t, typename Arg_t>
+template <typename Return_t, typename Class_t, typename... Arg_t>
 Return_t CallMemberFunction(Class_t* pInstance, 
-		Return_t (Class_t::*pFn)(Arg_t), Arg_t arg)
+		Return_t (Class_t::*pFn)(Arg_t...), Arg_t... args)
 {
-	return (pInstance->*pFn)(arg);
+	return (pInstance->*pFn)(args...);
 }
 
 class MyClass
@@ -18,9 +19,9 @@ public:
 	{
 	}
 
-	double Multiply(double value)
+	double Multiply(double value, double value2)
 	{
-		return value * m_multiplier;
+		return value * value2 * m_multiplier;
 	}
 
 private:
@@ -32,6 +33,6 @@ int main(int argc, char* argv[])
 {
 	MyClass myDoubler(2.);
 
-	std::cout << CallMemberFunction(&myDoubler,&MyClass::Multiply,2.) << std::endl;
+	std::cout << CallMemberFunction(&myDoubler,&MyClass::Multiply,2.,3.) << std::endl;
 }
 
