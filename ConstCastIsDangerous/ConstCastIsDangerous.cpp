d932b70173f9160b0c16620c01adcbc65e598a89
Jonathan Coe <jon@jbcoe.net>
2011-12-08 22:32:58 +0000

    Improved printed output and added non const member

--- ConstCastIsDangerous/ConstCastIsDangerous.cpp
+++ ConstCastIsDangerous/ConstCastIsDangerous.cpp
@@ -4,11 +4,21 @@ class MyClass
 {
 	public:
 
-		MyClass(int value) : m_fixedValue(value) {} 
+		MyClass(int value) : m_fixedValue(value), m_nonFixedValue(value) {} 
+
+		int m_nonFixedValue;
 
 		const int m_fixedValue;
 
-		static const int m_fixedStaticValue;
+		static const int m_fixedStaticValue; 
+
+		void printMe() const
+		{
+			std::cout << "  non fixed value: " << m_nonFixedValue << std::endl;
+			std::cout << "  fixed value: " << m_fixedValue << std::endl;
+			std::cout << "  static fixed value: " << m_fixedStaticValue << std::endl;
+			std::cout << std::endl;
+		}
 };
 
 const int MyClass::m_fixedStaticValue = 9;
@@ -16,10 +26,17 @@ const int MyClass::m_fixedStaticValue = 9;
 int main()
 {
 	const MyClass myInstance(9);
-	std::cout << myInstance.m_fixedValue << std::endl;
+	
+	std::cout << "const_cast on member variable of const object " << std::endl;
+	const_cast<int&>(myInstance.m_nonFixedValue) = 7;
+	myInstance.printMe();
+	
+	std::cout << "const_cast on const member variable of const object " << std::endl;
 	const_cast<int&>(myInstance.m_fixedValue) = 7;
-	std::cout << myInstance.m_fixedValue << std::endl;
-	const_cast<int&>(myInstance.m_fixedStaticValue) = 7;
-	std::cout << MyClass::m_fixedStaticValue<< std::endl;
+	myInstance.printMe();
+	
+	std::cout << "const_cast on const static class variable" << std::endl;
+	const_cast<int&>(MyClass::m_fixedStaticValue) = 7;
+	myInstance.printMe();
 }
 
