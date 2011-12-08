1cfec8da05cd17be3a978b42af08f8e982b170b4
Jonathan Coe <jon@jbcoe.net>
2011-12-08 20:10:31 +0000

    Illustrate the unsafe nature of const cast

---
+++ ConstCastIsDangerous/ConstCastIsDangerous.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class MyClass
+{
+	public:
+
+		MyClass(int value) : m_fixedValue(value) {} 
+
+		const int m_fixedValue;
+
+		static const int m_fixedStaticValue;
+};
+
+const int MyClass::m_fixedStaticValue = 9;
+
+int main()
+{
+	const MyClass myInstance(9);
+	std::cout << myInstance.m_fixedValue << std::endl;
+	const_cast<int&>(myInstance.m_fixedValue) = 7;
+	const_cast<int&>(myInstance.m_fixedStaticValue) = 7;
+	std::cout << myInstance.m_fixedValue << std::endl;
+}
+
