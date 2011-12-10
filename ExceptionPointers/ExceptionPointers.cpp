00c5dbc2b5317cee79a68594793718d87c0b59f0
Jonathan Coe <jon@jbcoe.net>
2011-12-10 18:30:34 +0000

    Exception pointers

---
+++ ExceptionPointers/ExceptionPointers.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <iostream>
+#include <vector>
+
+class Exception_A : public std::exception
+{
+};
+
+class Exception_B : public std::exception
+{
+};
+
+void Thrower(int i)
+{
+	if( (i % 2) == 0 )
+	{
+		throw Exception_A();
+	}
+	else
+	{
+		throw Exception_B();
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	int count = 4;
+	std::vector<std::exception_ptr> exceptionPointers(count);
+	for ( int i=0; i<count; ++i )
+	{
+		try 
+		{
+			Thrower(i);
+		}
+		catch ( ... )
+		{
+			exceptionPointers[i] = std::current_exception();
+		}
+	}
+
+  for ( int i=0; i<exceptionPointers.size(); ++i )
+	{
+		try
+		{
+			if ( ! ( exceptionPointers[i] == 0 ) )
+			{
+				std::rethrow_exception(exceptionPointers[i]);
+			}
+		}
+		catch ( const Exception_A& ea )
+		{
+			std::cerr << "Caught an exception of type A" << std::endl;
+		}
+		catch ( const Exception_B& eb )
+		{
+			std::cerr << "Caught an exception of type B" << std::endl;
+		}
+	}
+}
+
