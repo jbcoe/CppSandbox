96006c4cd7399978635e15579112c888d265d6a3
Jonathan Coe <jon@jbcoe.net>
2011-12-18 23:17:05 +0000

    Class member variable values can be specified

--- ConstructionImprovement/ConstructionImprovement.cpp
+++ ConstructionImprovement/ConstructionImprovement.cpp
@@ -2,24 +2,27 @@
 
 class MyClass
 {
-private:
+	private:
 
-	int m_iID = -1;
+		double m_rValue = 3.14159;
+		int m_iID = -1;
 
-public:
-	MyClass(int iID) : m_iID(iID) 
+	public:
+		MyClass(int iID) : m_iID(iID) 
 	{                      
 		std::cout << "Built and instance with ID: " << m_iID << std::endl; 
 	}
 
-	MyClass() : MyClass(42) 
+		MyClass() : MyClass(42) 
 	{
 		std::cout << "Delegated construction " << m_iID << std::endl; 
+		std::cout << "MyClass instance build with value : " 
+			<< m_rValue << std::endl;
 	}
 
-	~MyClass()
-	{
-	}
+		~MyClass()
+		{
+		}
 
 };
 
