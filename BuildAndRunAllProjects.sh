0036cdf88c4eb4f81e7990e2f973808fde2e8093
Jonathan Coe <jon@jbcoe.net>
2011-12-20 23:37:04 +0000

    Improved run all script

--- BuildAndRunAllProjects.sh
+++ BuildAndRunAllProjects.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 for x in $(for var in $(find . -iname Makefile) ; do echo $(basename $(dirname $var)) ; done ); 
-	do echo "== $x ==" ; echo ; cd $x ; make clean ; make ; cd .. ; echo ; 
+do clear ; echo "== $x ==" ; echo ; cd $x ; make clean ; make && echo [OK] ;
+	echo Press Enter to continue ; read line ; cd .. ; echo ; 
 done
