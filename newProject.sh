68cc1846fdf1d940bad08811a6597bde62df4894
Jonathan Coe <jon@jbcoe.net>
2011-12-29 18:36:45 +0000

    newProject script can handle sub directories

--- newProject.sh
+++ newProject.sh
@@ -1,26 +1,27 @@
 #/bin/bash
 
-PROJECT_NAME=$1
+PROJECT_DIR=$1
 
-if [ -e ${PROJECT_NAME} ] 
+PROJECT_NAME=$(echo $PROJECT_DIR | sed s#.*/##)
+
+if [ -e ${PROJECT_DIR} ] 
 then 
-	echo "A file called "${PROJECT_NAME}" exists"
+	echo "A file called "${PROJECT_DIR}" exists"
 	exit -1
 fi
 
-mkdir $PROJECT_NAME
+mkdir -p $PROJECT_DIR
 
-touch ${PROJECT_NAME}/${PROJECT_NAME}.cpp
+touch ${PROJECT_DIR}/${PROJECT_NAME}.cpp
 
-cat .MakefileTemplate | sed s/PROJECT_NAME/${PROJECT_NAME}/ > ${PROJECT_NAME}/Makefile
-cat .CPP_Template > ${PROJECT_NAME}/${PROJECT_NAME}.cpp
+cat .MakefileTemplate | sed s/PROJECT_NAME/${PROJECT_NAME}/ > ${PROJECT_DIR}/Makefile
+cat .CPP_Template > ${PROJECT_DIR}/${PROJECT_NAME}.cpp
 
-echo ${PROJECT_NAME}/${PROJECT_NAME} >> .gitignore
+echo ${PROJECT_DIR}/${PROJECT_NAME} >> .gitignore
 
 git add .gitignore
-git add ${PROJECT_NAME}/Makefile
-git add ${PROJECT_NAME}/${PROJECT_NAME}.cpp
+git add ${PROJECT_DIR}/Makefile
+git add ${PROJECT_DIR}/${PROJECT_NAME}.cpp
 
-
-echo Created project ${PROJECT_NAME}
+echo Created project ${PROJECT_NAME} in ${PROJECT_DIR}
 echo Changes pending but not yet committed
