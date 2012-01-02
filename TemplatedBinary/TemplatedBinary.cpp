3062d286860a147e76761cb3625b26ef8acff4c4
Jonathan Coe <jon@jbcoe.net>
2012-01-02 13:14:43 +0000

    added some output to templated binary project

--- TemplatedBinary/TemplatedBinary.cpp
+++ TemplatedBinary/TemplatedBinary.cpp
@@ -6,9 +6,7 @@ struct binary
 {
 	BOOST_STATIC_ASSERT(( N % 10 < 2));
 
-	static unsigned const value
-		= binary<N/10>::value * 2
-			+ N%10;
+	static unsigned const value = binary<N/10>::value * 2 + N%10;
 };
 
 template<>
@@ -19,5 +17,17 @@ struct binary<0>
 
 int main(int argc, const char* argv[])
 {
-	unsigned const seven = binary<111>::value;
+
+#define PRINT_AS_BINARY_AND_VALUE(theValue) \
+{\
+	std::cout << theValue << " is the binary representation of " << binary<theValue>::value << std::endl;\
+}
+
+	PRINT_AS_BINARY_AND_VALUE(111);
+	PRINT_AS_BINARY_AND_VALUE(1011);
+	PRINT_AS_BINARY_AND_VALUE(1101);
+	PRINT_AS_BINARY_AND_VALUE(101010);
+
+#undef PRINT_AS_BINARY_AND_VALUE	
+
 }
