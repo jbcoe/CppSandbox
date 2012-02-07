e82feabdf7927b789b6783a42fc4a48c0a3fd928
Jonathan Coe <jon@jbcoe.net>
2012-02-07 23:56:19 +0000

    Futures working with clang

---
+++ ASync/ASync.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <future>
+
+int evaluateSum (int a, int b)
+{
+	return a + b;
+}
+
+int main(int argc, char* argv[])
+{
+	auto result = std::async(evaluateSum,2,3);
+	std::cout << "2 + 3 = " << result.get() << std::endl;
+}
+
