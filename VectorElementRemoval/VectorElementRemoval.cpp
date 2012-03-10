4fbc87a04c0d1a02bf5ee8237532deea2c0ef970
Jonathan Coe <jon@jbcoe.net>
2012-03-10 17:35:30 +0000

    Vector removal checks to see if removing values is faster than skipping them

--- VectorElementRemoval/VectorElementRemoval.cpp
+++ VectorElementRemoval/VectorElementRemoval.cpp
@@ -2,56 +2,40 @@
 #include <iostream>                     
 #include <algorithm>                     
 
-
-class Copyable
-{
-public:
-	
-  static int iCopyCount;
-
-	Copyable(int i) : m_i(i) {}
-	
-	Copyable(const Copyable& t) : m_i(t.m_i) 
-	{
-		if ( iCopyCount != -1 )
-			++iCopyCount;
-	}
-
-  void operator = (const Copyable& t ) 
-	{
-		m_i = t.m_i;
-		if ( iCopyCount != -1 )
-			++iCopyCount;
-	}
-
-	operator int() { return m_i; }
-
-private:
-	
-	int m_i;
-
-};
-
-int Copyable::iCopyCount = -1;
-
 int main()
 {
-	std::vector<Copyable> myValues;
-  for ( int i=0; i<100; ++i )
+	std::vector<int> myValues;
+  for ( long int i=0L; i<100000000L; ++i )
 		myValues.push_back(i%3);
-
-	std::cout << &myValues[0] << std::endl;
 	
 	int iOriginalSize = myValues.size();
 
-	Copyable::iCopyCount = 0;
+#ifndef REMOVE_VALUES
+#ifndef CHECK_VALUES
+#error "define one of REMOVE_VALUES or CHECK_VALUES"
+#endif
+#endif
 
+#ifdef REMOVE_VALUES
+#ifdef CHECK_VALUES
+#error "define either REMOVE_VALUES or CHECK_VALUES"
+#endif
+#endif
+
+#ifdef REMOVE_VALUES
 	myValues.erase(std::remove_if(myValues.begin(),myValues.end(),[](int i) { return i == 2; }),myValues.end());
-
-	std::cout << &myValues[0] << std::endl;
-
-	std::cout << "Removing " << iOriginalSize - myValues.size() 
-		<< " values out of " << myValues.size() 
-		<< " took " << Copyable::iCopyCount << " copies" << std::endl;
+#endif
+	int sum = 0;
+  for ( unsigned int i=0; i<myValues.size(); ++i )
+	{
+#ifdef CHECK_VALUES
+		if ( myValues[i] != 2 )
+		{
+#endif
+			sum += myValues[i];
+#ifdef CHECK_VALUES 
+		}
+#endif
+	}
 
 }
