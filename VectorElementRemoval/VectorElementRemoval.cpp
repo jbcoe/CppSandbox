88dce14689de6fe5389661bd50716e02f000e7e4
Jonathan Coe <jon@jbcoe.net>
2012-03-12 20:11:18 +0000

    Check vs Remove - not so not so clear

--- VectorElementRemoval/VectorElementRemoval.cpp
+++ VectorElementRemoval/VectorElementRemoval.cpp
@@ -17,24 +17,28 @@
 int main()
 {
 	std::vector<int> myValues;
-  for ( long int i=0L; i<100000000L; ++i )
+	for ( long int i=0L; i<10000000L; ++i )
 		myValues.push_back(i%3);
-	
+
 	int iOriginalSize = myValues.size();
 #ifdef REMOVE_VALUES
 	myValues.erase(std::remove_if(myValues.begin(),myValues.end(),[](int i) { return i == 2; }),myValues.end());
 #endif
-	int sum = 0;
-  for ( unsigned int i=0; i<myValues.size(); ++i )
-	{
-#ifdef CHECK_VALUES
-		if ( myValues[i] != 2 )
-		{
-#endif
-			sum += myValues[i];
-#ifdef CHECK_VALUES 
-		}
-#endif
-	}
 
+	const int iterations = 100;
+	for ( int iteration=0; iteration < iterations; ++iteration )
+	{
+		int sum = 0;
+		for ( unsigned int i=0; i<myValues.size(); ++i )
+		{
+#ifdef CHECK_VALUES
+			if ( myValues[i] != 2 )
+			{
+#endif
+				sum += myValues[i];
+#ifdef CHECK_VALUES 
+			}
+#endif
+		}                                                 
+	}
 }
