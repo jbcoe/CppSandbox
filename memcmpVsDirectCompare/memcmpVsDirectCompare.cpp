a4b9d0a74c3139e1dc3be1dedd0ad489f5f3cd3b
Jonathan Coe <jon@jbcoe.net>
2012-05-28 23:19:59 +0100

    tweaks

--- memcmpVsDirectCompare/memcmpVsDirectCompare.cpp
+++ memcmpVsDirectCompare/memcmpVsDirectCompare.cpp
@@ -6,8 +6,8 @@
 using namespace boost;
 using namespace posix_time;
 
-typedef double COMPARED_TYPE;
-static const COMPARED_TYPE COMPARED_VALUE = 1.0;
+typedef int COMPARED_TYPE;
+static const COMPARED_TYPE COMPARED_VALUE = 1;
 
 class Timer
 {
@@ -39,49 +39,42 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	int size = boost::lexical_cast<size_t>(argv[1]);
+	size_t size = boost::lexical_cast<size_t>(argv[1]);
 	int sum_of_cmps = 0; //Here to stop the optimizer stripping away code
-	
-	{
-		std::vector<COMPARED_TYPE> numbers(size,COMPARED_VALUE);
-		std::vector<COMPARED_TYPE> moreNumbers(size,COMPARED_VALUE);
 
-		{
-			Timer t("memcmp");
-			int cmp = memcmp(&numbers[0],&moreNumbers[0],size*sizeof(COMPARED_TYPE));
-			sum_of_cmps += cmp;
-		}
+	std::vector<COMPARED_TYPE> numbers(size,COMPARED_VALUE);
+	std::vector<COMPARED_TYPE> moreNumbers(size,COMPARED_VALUE);
+
+	{
+		Timer t("memcmp");
+		int cmp = memcmp(&numbers[0],&moreNumbers[0],size*sizeof(COMPARED_TYPE));
+		sum_of_cmps += cmp;
 	}
 
+	numbers.assign(size,COMPARED_VALUE);
+	moreNumbers.assign(size,COMPARED_VALUE);
 	{
-		std::vector<COMPARED_TYPE> numbers(size,COMPARED_VALUE);
-		std::vector<COMPARED_TYPE> moreNumbers(size,COMPARED_VALUE);
-
+		Timer t("explicit compare");
+		int cmp = 0;
+		for (size_t i=0; i<size; ++i)
 		{
-			Timer t("explicit compare");
-			int cmp = 0;
-			for (size_t i=0; i<size; ++i)
+			if ( numbers[i] != moreNumbers[i] )
 			{
-				if ( numbers[i] != moreNumbers[i] )
-				{
-					cmp = 1;
-					break;
-				}
+				cmp = 1;
+				break;
 			}
-			sum_of_cmps += cmp;
 		}
+		sum_of_cmps += cmp;
 	}
 
+	numbers.assign(size,COMPARED_VALUE);
+	moreNumbers.assign(size,COMPARED_VALUE);
 	{
-		std::vector<COMPARED_TYPE> numbers(size,COMPARED_VALUE);
-		std::vector<COMPARED_TYPE> moreNumbers(size,COMPARED_VALUE);
-
-		{
-			Timer t("std equals");
-			int cmp = std::equal(numbers.begin(),numbers.end(),moreNumbers.begin());
-			sum_of_cmps += cmp;
-		}
+		Timer t("std equals");
+		int cmp = std::equal(numbers.begin(),numbers.end(),moreNumbers.begin());
+		sum_of_cmps += cmp;
 	}
-	return sum_of_cmps;
+
+	return sum_of_cmps==5;
 }
 
