5b2dea6908a4622f1fe4d5799c9bf2a7a4bdb6d0
Jonathan Coe <jon@jbcoe.net>
2012-06-02 00:23:08 +0100

    First go at atomics. Not really needed(?)

---
+++ Atomics/Atomics.cpp
@@ -0,0 +1,26 @@
+#include <atomic>
+#include <iterator>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <future>
+
+int main(int argc, char* argv[])
+{
+	std::atomic<size_t> completionCount;
+	completionCount = 0;
+
+	std::vector<std::future<size_t>> futures(100);
+	for ( size_t i=0; i<100; ++i )
+	{
+  	futures[i] = std::async([&completionCount](){return ++completionCount;});
+	}
+
+	for ( size_t i=0; i<futures.size(); ++i )
+	{
+		std::cout << futures[i].get();
+		if ( i<futures.size()-1 ) std::cout << ", ";
+	}
+	std::cout << std::endl;
+}
+
