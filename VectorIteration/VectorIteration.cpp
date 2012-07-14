3294d5edb7a0a217a21b399978decfdcc444d51b
Jonathan Coe <jon@jbcoe.net>
2012-07-14 11:31:23 +0100

    Zip iterators. Optimisation level is now configurable

---
+++ VectorIteration/VectorIteration.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <Common/Timer.h>
+#include <vector>
+#include <cstdlib>
+#include <boost/lexical_cast.hpp>
+#include <random>
+#include <boost/tuple/tuple.hpp>
+#include <boost/iterator/zip_iterator.hpp>
+
+class MemBlock
+{
+	public:
+		template <typename Generator_t>
+		MemBlock(size_t size, Generator_t& g) : m_size(size) 
+	{
+		m_rawMemory = malloc(size);
+		for (size_t i=0; i<(size)/sizeof(size_t); ++i )
+		{
+			*(size_t*)(m_rawMemory+i) = g();
+		}		
+	}
+
+		~MemBlock()
+		{
+			free(m_rawMemory);
+		}
+		size_t m_size;
+		void* m_rawMemory;
+};
+
+	template <typename Generator_t>
+double* GetDoublePointer(MemBlock& m, size_t length, Generator_t& g)
+{
+	auto position = g();
+	while ( (position+length) >= m.m_size )
+		position = g();
+
+	return reinterpret_cast<double*>(m.m_rawMemory+position);
+}
+
+int main(int argc, char* argv[])
+{
+	size_t size = boost::lexical_cast<size_t>(argv[1]);
+
+	std::uniform_int_distribution<size_t> distribution(0,size/10);
+	std::mt19937 engine;
+	auto generator = [&]{return distribution(engine);};
+
+	MemBlock m(size,generator);
+	
+	size_t length = size/10; 
+
+	auto theFunction = [](double a, double b, double c, double d) { return (a+b)/(c+d); };
+
+	double sum = 0.0;
+	{
+		Timer t("Simple loops");
+		for ( size_t pass=0; pass<100; ++pass)
+		{
+			double* ws = GetDoublePointer(m,length,generator);
+			double* xs = GetDoublePointer(m,length,generator);
+			double* ys = GetDoublePointer(m,length,generator);
+			double* zs = GetDoublePointer(m,length,generator);
+
+			for ( int i=0; i<length; ++i )
+			{
+				sum += theFunction(ws[i],xs[i],ys[i],zs[i]);
+			}
+		}
+	}
+
+	std::cout << sum << std::endl; 
+
+	double sum2 = 0.0;
+	{
+		double* ws = GetDoublePointer(m,length,generator);
+		double* xs = GetDoublePointer(m,length,generator);
+		double* ys = GetDoublePointer(m,length,generator);
+		double* zs = GetDoublePointer(m,length,generator);
+
+		Timer t("Zip iterators");
+		std::for_each(
+				boost::make_zip_iterator(
+					boost::make_tuple(ws,xs,ys,zs)
+					),
+				boost::make_zip_iterator(
+					boost::make_tuple(ws+length,xs+length,ys+length,zs+length)
+					),
+				[&](const boost::tuple<double,double,double,double>& t)
+				{
+				sum2 += theFunction(t.get<0>(),t.get<1>(),t.get<2>(),t.get<3>());
+				}
+				);
+	}
+
+	std::cout << sum2 << std::endl; 
+}
--- compilerSettings.mk
+++ compilerSettings.mk
@@ -1,3 +1,4 @@
 CC = g++-mp-4.7
-CFLAGS = -Wall -Winline -Wwrite-strings -Wno-unused -O0 -std=c++0x -I../
+OPT=0
+CFLAGS = -Wall -Winline -Wwrite-strings -Wno-unused -O${OPT} -std=c++0x -I../
 LDFLAGS = 
