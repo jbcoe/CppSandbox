ac0ff896f4d5c4c607c85b1cdab7b5166651de2d
Jonathan Coe <jon@jbcoe.net>
2012-08-10 00:22:54 +0100

    Improved Lambda layout

--- LambdaFunctions/LambdaFunctions.cpp
+++ LambdaFunctions/LambdaFunctions.cpp
@@ -7,10 +7,12 @@ int main(int argc, char* argv[])
 	std::vector<int> some_list = {1,2,3,4,5,6,7,8,9};
 	int count = 0;
 	int sum = 0;
-	std::for_each(some_list.begin(), some_list.end(), 
-			[&count,&sum](int x) 
-			{sum += x; ++count;}
-			);
+	
+	std::for_each(some_list.begin(), some_list.end(), [&count,&sum](int x) 
+	{
+		sum += x; 
+		++count;
+	});
 
 	std::cout << "Sum: " << sum << " count: "<< count << std::endl;
 }
