3863f0dcac472f764a4255c8f777d2d672525eb5
Jonathan Coe <jon@jbcoe.net>
2012-08-15 21:17:03 +0100

    Timer is properly scoped and gives good results

--- FalseSharing/FalseSharing.cpp
+++ FalseSharing/FalseSharing.cpp
@@ -1,45 +1,45 @@
 // See http://herbsutter.com/2009/05/15/effective-concurrency-eliminate-false-sharing/
 
-#include <iostream>
+#include <Common/Timer.h>
 #include <algorithm>
+#include <boost/lexical_cast.hpp>
+#include <iostream>
 #include <random>
-#include <vector>
 #include <tbb/task_group.h>
 #include <tbb/task_scheduler_init.h>
-#include <boost/lexical_cast.hpp>
-#include <Common/Timer.h>
+#include <vector>
 
 class SquareMatrix
 {
 	public:
-	SquareMatrix(size_t dim) : m_data(dim*dim, 0), m_dim(dim) {}
-	double* operator[](size_t i)
-	{
-		return &m_data[i*m_dim];
-	}
+		SquareMatrix(size_t dim) : m_data(dim*dim, 0), m_dim(dim) {}
+		double* operator[](size_t i)
+		{
+			return &m_data[i*m_dim];
+		}
 
-	std::vector<double>::iterator begin() { return m_data.begin(); }
-	std::vector<double>::iterator end() { return m_data.end(); }
+		std::vector<double>::iterator begin() { return m_data.begin(); }
+		std::vector<double>::iterator end() { return m_data.end(); }
 
 	private:
-	std::vector<double> m_data;
-	size_t m_dim;
+		std::vector<double> m_data;
+		size_t m_dim;
 };
 
 int main(int argc, char* argv[])
 {
-  size_t THREADS = 4;
+	size_t THREADS = 4;
 	if ( argc > 1 )
 		THREADS = boost::lexical_cast<size_t>(argv[1]);
-	
+
 	size_t dim = 1024;
 	if ( argc > 2 )
 		dim = boost::lexical_cast<size_t>(argv[2]);
-	
+
 	std::uniform_real_distribution<double> distribution(-1,1);
 	std::mt19937 engine;
 	auto generator = [&]{return distribution(engine);};
-	
+
 	SquareMatrix m(dim);
 	std::generate(m.begin(),m.end(),generator);
 
@@ -47,33 +47,33 @@ int main(int argc, char* argv[])
 	std::vector<size_t> perThreadCount(THREADS,0);
 	tbb::task_scheduler_init tsInit(THREADS);
 	tbb::task_group tg;
-	
-	Timer t("Time count");
-	
-	for ( size_t iTask=0; iTask<THREADS; ++iTask )
 	{
-  	tg.run([=,&perThreadCount,&m]
-				{
-					size_t taskSize = dim/THREADS;
-					size_t taskStart = iTask*taskSize;
-					size_t taskEnd = std::min(taskStart+taskSize, dim);
-          size_t count = 0;
-					for (size_t i=taskStart; i<taskEnd; ++i)
+		Timer t("Time count");
+
+		for ( size_t iTask=0; iTask<THREADS; ++iTask )
+		{
+			tg.run([=,&perThreadCount,&m]
 					{
-						for (size_t j=0; j<dim; ++j)
+						size_t taskSize = dim/THREADS;
+						size_t taskStart = iTask*taskSize;
+						size_t taskEnd = std::min(taskStart+taskSize, dim);
+						size_t count = 0;
+						for (size_t i=taskStart; i<taskEnd; ++i)
 						{
-            	if (m[i][j]>=0.0)
+							for (size_t j=0; j<dim; ++j)
 							{
-							  ++perThreadCount[iTask];
-								//++count;
+								if (m[i][j]>=0.0)
+								{
+									++perThreadCount[iTask]; // Causes false sharing
+									//++count; //This prevents false sharing
+								}
 							}
 						}
-					}
-					// perThreadCount[iTask] = count;
-				});
+					//perThreadCount[iTask] = count; // This prevents false sharing
+					});
+		}
+		tg.wait();
 	}
-	tg.wait();
-
 	auto positiveCount = std::accumulate(perThreadCount.begin(),perThreadCount.end(),0);
 	std::cout << dim << " by " << dim << " matrix has " << positiveCount << " positive entries = "
 		<< 100.0*positiveCount/(dim*dim) << "%" << std::endl;
