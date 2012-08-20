c4de973a9f99cea7ce73589f40e03ae20866a047
Jonathan Coe <jon@jbcoe.net>
2012-08-21 00:19:24 +0100

    Private visitor compiles and runs

---
+++ PrivateVisitor/PrivateVisitor.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+class IVisitor;
+
+class IVisitable
+{
+	public:
+		virtual void Accept(IVisitor& visitor) const = 0;
+};
+
+class VisitableA;
+class VisitableB;
+
+class IVisitor
+{
+	public:
+		virtual void Visit(const VisitableA& a) = 0;
+		virtual void Visit(const VisitableB& b) = 0;
+};
+
+class VisitableA : public IVisitable
+{
+	public:
+		virtual void Accept(IVisitor& visitor) const
+		{
+			visitor.Visit(*this);
+		}
+};
+
+class VisitableB : public IVisitable
+{
+	public:
+		virtual void Accept(IVisitor& visitor) const
+		{
+			visitor.Visit(*this);
+		}
+};
+
+class PrivateVisitor : private IVisitor
+{
+	public:
+		PrivateVisitor(IVisitable& v)
+		{
+			v.Accept(*this);	
+		}
+
+	private:
+
+		virtual void Visit(const VisitableA& a)
+		{
+			std::cout << "I saw A\n";
+		}
+
+		virtual void Visit(const VisitableB& b)
+		{
+			std::cout << "I saw B\n";
+		}
+};
+
+int main(int argc, char* argv[])
+{
+	VisitableA a;
+	VisitableB b;
+
+	PrivateVisitor p_a(a);
+	PrivateVisitor p_b(b);
+}
+
