d523c11e37c1adf5b83a698e4329ad10f5e2bab2
Jonathan Coe <jon@jbcoe.net>
2012-08-27 21:57:00 +0100

    simplified tuple example

--- Tuple/Tuple.cpp
+++ Tuple/Tuple.cpp
@@ -2,27 +2,19 @@
 #include <vector>
 #include <tuple>
 
-typedef std::tuple <int,double,std::string,std::vector<double>> testTuple;
-
 int main(int argc, char* argv[])
 {
-	testTuple tuple;
-	auto& v0 = std::get<0>(tuple);
-	auto& v1 = std::get<1>(tuple);
-	auto& v2 = std::get<2>(tuple);
-	auto& v3 = std::get<3>(tuple);
-	
-	v0 = 0;
-	v1 = 3.14159;
-	v2 = "The news";
-  v3.push_back(2.718281);
-  v3.push_back(0.0);
-  v3.push_back(5.0);
+	std::vector<double> v;
+	v.push_back(2.718281);
+  v.push_back(0.0);
+  v.push_back(5.0);
 
-	std::cout << "Tuple data: " << std::get<0>(tuple) << std::endl; 
-	std::cout << "Tuple data: " << std::get<1>(tuple) << std::endl; 
-	std::cout << "Tuple data: " << std::get<2>(tuple) << std::endl; 
-	for ( const auto& v : std::get<3>(tuple) )
+  auto testTuple = std::make_tuple(0,3.14159,"The news",v);
+
+	std::cout << "Tuple data: " << std::get<0>(testTuple) << std::endl; 
+	std::cout << "Tuple data: " << std::get<1>(testTuple) << std::endl; 
+	std::cout << "Tuple data: " << std::get<2>(testTuple) << std::endl; 
+	for ( const auto& v : std::get<3>(testTuple) )
 	{
 		std::cout << "Tuple vector data: " << v << std::endl;
 	}
