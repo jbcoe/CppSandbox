4422160ccefb18a7cbde598e897abaa21db56a69
Jonathan Coe <jon@jbcoe.net>
2012-09-19 23:30:07 +0100

    Added inheritance anc composition

--- OrderingOfConstructorsAndDestructors/OrderingOfConstructorsAndDestructors.cpp
+++ OrderingOfConstructorsAndDestructors/OrderingOfConstructorsAndDestructors.cpp
@@ -17,9 +17,28 @@ class A
 		int m_id;
 };
 
+class B : public A
+{
+	public:
+		B(int id) : A(id), m_id(id), m_a(id+1) 
+		{
+			std::cout << "An object of type B with ID " << m_id << " has been created\n";
+		}                                                       
+		
+		~B() 
+		{
+			std::cout << "An object of type B with ID " << m_id << " has been destroyed\n";
+		}                                                    
+
+	private:
+		int m_id;
+		A m_a;
+};
+
 int main(int argc, char* argv[])
 {
 	A a1(1);
 	A a2(2);
+	B b(3);
 }
 
