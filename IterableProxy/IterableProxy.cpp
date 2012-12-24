c53162e211696ed70920e48e1c085f40dd453a94
Jonathan Coe <jon@jbcoe.net>
2012-12-24 21:21:29 +0100

    more detail added

--- IterableProxy/IterableProxy.cpp
+++ IterableProxy/IterableProxy.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <cassert>
 #include <memory>
 #include <vector>
 
-class ITiger{};
-class IBear {};
-class ILion {};
+class IAnimal 
+{ 
+	public:
+	virtual ~IAnimal(){}
+	virtual const char* makeNoise() const = 0; 
+};
+
+class Tiger : public IAnimal { public: const char* makeNoise() const { return "Purr"; } };
+class Bear : public IAnimal { public: const char* makeNoise() const { return "Growl"; } };
+class Lion : public IAnimal { public: const char* makeNoise() const { return "Roar"; } };
 
 class IZoo
 {
@@ -13,13 +21,13 @@ class IZoo
  	virtual ~IZoo() {}
 
 	virtual size_t BearCount() const = 0;
-	virtual const IBear& Bear(size_t index) const = 0;
+	virtual const Bear* GetBear(size_t index) const = 0;
 	
 	virtual size_t TigerCount() const = 0;
-	virtual const ITiger& Tiger(size_t index) const = 0;
+	virtual const Tiger* GetTiger(size_t index) const = 0;
 	
 	virtual size_t LionCount() const = 0;
-	virtual const ILion& Lion(size_t index) const = 0;
+	virtual const Lion* GetLion(size_t index) const = 0;
 };
 
 class Zoo : public IZoo
@@ -28,22 +36,22 @@ class Zoo : public IZoo
 
   Zoo() 
 	{
-		std::generate_n(std::back_inserter(m_lions), 10, [](){return std::unique_ptr<ILion>();});
+		std::generate_n(std::back_inserter(m_lions), 10, [](){return std::unique_ptr<Lion>(new Lion());});
 	}
 
 	size_t BearCount() const { return m_bears.size(); }
-	const IBear& Bear(size_t index) const { return *m_bears[index]; }
+	const Bear* GetBear(size_t index) const { return m_bears[index].get(); }
 	
 	size_t TigerCount() const { return m_tigers.size(); }
-	const ITiger& Tiger(size_t index) const { return *m_tigers[index]; }
+	const Tiger* GetTiger(size_t index) const { return m_tigers[index].get(); }
 	
 	size_t LionCount() const { return m_lions.size(); }
-	const ILion& Lion(size_t index) const { return *m_lions[index]; }
+	const Lion* GetLion(size_t index) const { return m_lions[index].get(); }
 
 	private:
-		std::vector<std::unique_ptr<ILion>> m_lions;
-		std::vector<std::unique_ptr<ITiger>> m_tigers;
-		std::vector<std::unique_ptr<IBear>> m_bears;
+		std::vector<std::unique_ptr<Lion>> m_lions;
+		std::vector<std::unique_ptr<Tiger>> m_tigers;
+		std::vector<std::unique_ptr<Bear>> m_bears;
 };
 
 template <typename Object_t, typename Get_f, typename Count_f>
@@ -56,16 +64,16 @@ class IterableProxy
    class iterator
 	 {
 		 public:
-      iterator(Get_f f_get, size_t index=0) : m_index(index), m_f_get{f_get} {}  
+      iterator(Get_f& f_get, size_t index=0) : m_index(index), m_f_get{f_get} { }  
       
-      const Object_t& operator * () { return *m_f_get(m_index); }
+      const Object_t* operator * () { return m_f_get(m_index); }
       iterator& operator++() { ++m_index; return *this; }
 			bool operator == (const iterator& i) const { return m_index == i.m_index; }
 			bool operator != (const iterator& i) const { return m_index != i.m_index; }
 
 		private:
 			size_t m_index;
-	 		Get_f m_f_get;
+	 		Get_f& m_f_get;
 	 };
 
 	 iterator begin() { return iterator(m_f_get); }
@@ -87,13 +95,17 @@ make_iterable_proxy(Get_f f_get, Count_f f_count)
 int main(int argc, char* argv[])
 {
 	Zoo myZoo;
-	auto iterableLions = make_iterable_proxy<ILion>(
-			[&myZoo](size_t index){ return &myZoo.Lion(index); }, 
+	
+	assert(myZoo.LionCount() == 10);
+	assert(myZoo.GetLion(0));
+
+	auto iterableLions = make_iterable_proxy<Lion>(
+			[&myZoo](size_t index){ return myZoo.GetLion(index); }, 
 			[&myZoo](){ return myZoo.LionCount(); });
 
-	for ( const auto& myLion : iterableLions )
+	for ( auto myLion : iterableLions )
 	{
-		std::cout << "Roar!" << std::endl;
+		std::cout << myLion->makeNoise() << std::endl;
 	}
 }
 
