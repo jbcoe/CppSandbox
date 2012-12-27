5c340597c0a3cfc095f37da96deea45e49a878fe
Jonathan Coe <jon@jbcoe.net>
2012-12-27 22:17:35 +0100

    Lazy initialized member variable

--- LazyInitializer/LazyInitializer.cpp
+++ LazyInitializer/LazyInitializer.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-template <typename Value_t, typename Initialized_t>
+template <typename Value_t, typename Initializer_t>
 class LazyInitialized
 {
 	public:
-		LazyInitialized(Initialized_t i) : m_i(i), m_v(), m_needInit(true) {}
+		LazyInitialized(Initializer_t i) : m_i(i), m_v(), m_needInit(true) {}
 		operator Value_t() 
 		{ 
 			if ( m_needInit ) m_v = m_i();
@@ -13,22 +13,37 @@ class LazyInitialized
 		}
 
 	private:
-		Initialized_t m_i;
+		Initializer_t m_i;
 		Value_t m_v;
 		bool m_needInit;
 };
 
-template<typename Value_t, typename Initialized_t>
-LazyInitialized<Value_t,Initialized_t> 
-make_lazy_initialized(Initialized_t i) 
+template<typename Value_t, typename Initializer_t>
+LazyInitialized<Value_t,Initializer_t> 
+make_lazy_initialized(Initializer_t i) 
 {
-	return LazyInitialized<Value_t,Initialized_t>(i);
+	return LazyInitialized<Value_t,Initializer_t>(i);
 }
+
+struct ClassWithLazyMembers
+{
+	LazyInitialized<double, std::function<double (void)> > m_baseLazyValue;
+	LazyInitialized<double, std::function<double (void)> > m_lazyValue;
+  
+	ClassWithLazyMembers() : 
+		m_baseLazyValue{[]()->double{return 2.0;}},
+		m_lazyValue{[this]()->double{std::cout << "Built member variable" << std::endl; return 2.0 * this->m_baseLazyValue;}} {}
+	
+};
 
 int main(int argc, char* argv[])
 {
 	auto x = make_lazy_initialized<double>([]()->double{std::cout << "Built" << std::endl; return 2.0;});
 	std::cout << x << std::endl;
 	std::cout << x << std::endl;
+
+	ClassWithLazyMembers c;
+	std::cout << c.m_lazyValue << std::endl;
+	std::cout << c.m_lazyValue << std::endl;
 }
 
