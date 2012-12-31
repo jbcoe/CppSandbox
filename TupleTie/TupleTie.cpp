1e10ad48c38c1340ebb5b0d9a5e38cd933cdbaa7
Jonathan Coe <jon@jbcoe.net>
2012-12-31 18:30:58 +0000

    std::tie is nice

--- TupleTie/TupleTie.cpp
+++ TupleTie/TupleTie.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <tuple>
+
+using namespace std;
+
+tuple<int,int,double> ReturnTuple()
+{
+	return make_tuple(1,2,3.14159);
+}
 
 int main(int argc, char* argv[])
 {
+	int myInt = 0;
+	double myDouble = 0.0;
+
+	tie(myInt, ignore, myDouble) = ReturnTuple();
+
+	cout << myInt << " " << myDouble << endl;
 }
 
