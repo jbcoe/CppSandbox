e494ae3133cae666bb95221505187b5e533881e4
Jonathan Coe <jon@jbcoe.net>
2012-12-31 20:15:23 +0000

    constexpr for initialisation of static member

--- ConstructionImprovement/ConstructionImprovement.cpp
+++ ConstructionImprovement/ConstructionImprovement.cpp
@@ -2,32 +2,34 @@
 
 class MyClass
 {
-	private:
-
+	public:
 		double m_rValue = 3.14159;
 		int m_iID = -1;
 
-	public:
 		MyClass(int iID) : m_iID(iID) 
-	{                      
-		std::cout << "Built and instance with ID: " << m_iID << std::endl; 
-	}
+		{                      
+			std::cout << "Built instance with ID: " << m_iID << std::endl; 
+		}
 
 		MyClass() : MyClass(42) 
-	{
-		std::cout << "Delegated construction " << m_iID << std::endl; 
-		std::cout << "MyClass instance build with value : " 
+		{
+			std::cout << "Delegated construction " << m_iID << std::endl; 
+			std::cout << "MyClass instance build with value : " 
 			<< m_rValue << std::endl;
-	}
+		}
 
 		~MyClass()
 		{
 		}
 
+		static constexpr int s_iFive = 5;
 };
 
 int main(int argc, char* argv[])
 {
 	MyClass myInstance;
+	std::cout << "\nmyInstance.m_rValue = " << myInstance.m_rValue << std::endl;
+	std::cout << "myInstance.m_iID = " << myInstance.m_iID << std::endl;
+	std::cout << "MyClass.s_iFive = " << MyClass::s_iFive << std::endl;
 }
 
