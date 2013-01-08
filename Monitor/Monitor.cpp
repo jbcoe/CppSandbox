bc75197a19d9030a2beae3fd25bc2ba7128387c1
Jonathan B Coe <jon@jbcoe.net>
2013-01-08 11:00:55 +0000

    Update Monitor/Monitor.cpp
    DW pointed out that const Monitor is a bit pointless

--- Monitor/Monitor.cpp
+++ Monitor/Monitor.cpp
@@ -16,13 +16,6 @@ public:
 	Monitor(T _t) : t(_t) {}
 
 	template <typename F>
-	auto operator()(F f) const -> decltype(f(t))
-	{
-		std::lock_guard<std::mutex> l(m);
-		return f(t);
-	}
-	
-	template <typename F>
 	auto operator()(F f) -> decltype(f(t))
 	{
 		std::lock_guard<std::mutex> l(m);
@@ -32,14 +25,6 @@ public:
 
 int main(int argc, char* argv[])
 {
-	Monitor<std::string> s("Locked hello world");
-	s([](std::string& s){ s += " with Monitors"; });
-	s([](std::string& s){ std::cout << s << std::endl; });
-  
-	const Monitor<std::string> cs("const Locked hello world");
-	//cs([](std::string& s){ s += " with Monitors"; });  Won't compile as we can't call non const functions on a const Monitor
-	cs([](const std::string& s){ std::cout << s << " with Monitors" << std::endl; });
-
 	Monitor<std::ostream&> async_ios(std::ref(std::cout));
 	
 	std::vector<std::thread> threads;
