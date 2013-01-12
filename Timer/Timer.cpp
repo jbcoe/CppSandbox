e4e1c615a9289a5368d9f95642b78701bfedd130
Jonathan Coe <jon@jbcoe.net>
2013-01-12 11:58:19 +0000

    Timer can now replace that in common

--- Timer/Timer.cpp
+++ Timer/Timer.cpp
@@ -8,16 +8,16 @@ using namespace boost;
 using namespace posix_time;
 
 template <typename F_t>
-class Timer
+class TTimer
 {
 public:
 
-	Timer(F_t f_) : f(f_) 
+	TTimer(F_t f_) : f(f_) 
 	{
 		start = microsec_clock::local_time();
 	}
 
- 	~Timer()
+ 	~TTimer()
  	{
  		auto end = microsec_clock::local_time();
  		f((end-start).total_milliseconds());
@@ -28,6 +28,7 @@ private:
  	F_t f;
  	ptime start;
 };
+
 
 struct TimingReport
 {
@@ -40,18 +41,26 @@ private:
 };
 
 template <typename F_t>
-Timer<F_t> make_timer(F_t f)
+TTimer<F_t> make_timer(F_t f)
 {
-	return Timer<F_t>(f);
+	return TTimer<F_t>(f);
 }
 
-Timer<TimingReport> make_timer(const char* s) { return make_timer(TimingReport(s)); }
-Timer<TimingReport> make_timer(const std::string& s) { return make_timer(TimingReport(s)); }
+TTimer<TimingReport> make_timer(const char* s) { return make_timer(TimingReport(s)); }
+TTimer<TimingReport> make_timer(const std::string& s) { return make_timer(TimingReport(s)); }
+
+class Timer : TTimer<TimingReport>
+{
+	public:
+	Timer(const char* s) : TTimer(TimingReport(s)) {}
+	Timer(const std::string s) : TTimer(TimingReport(s)) {}
+};
 
 int main(int argc, char* argv[])
 {
 	auto t(make_timer([](long t){std::cout << "Time passed: " << t << std::endl; }));
-	auto t2(make_timer("Build vector"));
+	auto t2 = make_timer("Build vector");
+	Timer t3("Another timer");
 	std::vector<int> v(10000000);
 	assert(std::find(v.begin(), v.end(), 9) == v.end());
 }
