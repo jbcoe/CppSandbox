d833ba9cb9894962c5081abf4d8ff28163cc91fe
Jonathan Coe <jon@jbcoe.net>
2013-01-12 09:06:30 +0000

    Timer cpp now needs a void(long) function

--- Timer/Timer.cpp
+++ Timer/Timer.cpp
@@ -20,7 +20,7 @@ public:
  	~Timer()
  	{
  		auto end = microsec_clock::local_time();
- 		f(start, end);
+ 		f((end-start).total_milliseconds());
  	}
 
 private:
@@ -37,8 +37,8 @@ Timer<F_t> make_timer(F_t f)
 
 int main(int argc, char* argv[])
 {
-	auto t(make_timer([](ptime b, ptime e){std::cout << "Time passed: " << e-b << std::endl; }));
-	std::vector<int> v(10000);
+	auto t(make_timer([](long t){std::cout << "Time passed: " << t << std::endl; }));
+	std::vector<int> v(10000000);
 	assert(std::find(v.begin(), v.end(), 9) == v.end());
 }
 
