9eb1aef2edb38b763e2a2c57c7e6b9e8c0978724
Jonathan Coe <jon@jbcoe.net>
2013-01-12 12:01:21 +0000

    Improved timer now in common

--- Common/Timer.h
+++ Common/Timer.h
@@ -1,27 +1,55 @@
+#include <iostream>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/lexical_cast.hpp>
 
 using namespace boost;
 using namespace posix_time;
 
-class Timer
+template <typename F_t>
+class TTimer
 {
-	public:
+public:
 
-		Timer(const char* eventName) : m_event(eventName) 
+	TTimer(F_t f_) : f(f_) 
 	{
-		m_start = microsec_clock::local_time();
+		start = microsec_clock::local_time();
 	}
 
-		~Timer()
-		{
-			m_end = microsec_clock::local_time();
-			std::cout << m_event << ' ' << (m_end - m_start).total_milliseconds() << std::endl;
-		}
+ 	~TTimer()
+ 	{
+ 		auto end = microsec_clock::local_time();
+ 		f((end-start).total_milliseconds());
+ 	}
 
-	private:
+private:
 
-		std::string m_event;
-		ptime m_start;
-		ptime m_end;
+ 	F_t f;
+ 	ptime start;
+};
+
+
+struct TimingReport
+{
+  TimingReport(const std::string& e) : event(e) {}
+
+  void operator()(long t) const { std::cout << event << " (ms) " << t <<std::endl; }
+
+private:
+	std::string event;
+};
+
+template <typename F_t>
+TTimer<F_t> make_timer(F_t f)
+{
+	return TTimer<F_t>(f);
+}
+
+TTimer<TimingReport> make_timer(const char* s) { return make_timer(TimingReport(s)); }
+TTimer<TimingReport> make_timer(const std::string& s) { return make_timer(TimingReport(s)); }
+
+class Timer : TTimer<TimingReport>
+{
+	public:
+	Timer(const char* s) : TTimer(TimingReport(s)) {}
+	Timer(const std::string s) : TTimer(TimingReport(s)) {}
 };
