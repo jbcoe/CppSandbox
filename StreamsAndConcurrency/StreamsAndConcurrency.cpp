8ad27b2f09d2bc08ed0128d3755624d5ae94d42a
Jonathan Coe <jon@jbcoe.net>
2013-01-12 12:32:40 +0000

    Concurrency test gets effective thread count

--- StreamsAndConcurrency/StreamsAndConcurrency.cpp
+++ StreamsAndConcurrency/StreamsAndConcurrency.cpp
@@ -8,12 +8,12 @@
 template <typename TaskGenerator_t>
 void RunTasks(size_t tasks, TaskGenerator_t gen)
 {
+
+	long firstRunTime = 0;
+
   for (size_t i=1; i<=16; ++i)
 	{
-		std::stringstream ss;
-		ss << i << '\t';
-
-    Timer t(ss.str().c_str());
+    auto t = make_timer([&firstRunTime,i ](long t){ if (i==1) firstRunTime=t; std::cout << i << ' ' << (double)firstRunTime/t << std::endl;});
 
 		tbb::task_scheduler_init tsInit(i);
 		tbb::task_group tg;
@@ -23,6 +23,7 @@ void RunTasks(size_t tasks, TaskGenerator_t gen)
 		}
 		tg.wait();
 	}
+	std::cout << std::endl;
 }
 
 int main(int argc, char* argv[])
@@ -34,7 +35,7 @@ int main(int argc, char* argv[])
 		{
 			double r = 3.1;
 			double x = 0.6;
-			for (int i=0; i<100000; ++i)
+			for (int i=0; i<10000; ++i)
 			{
 				x = r * x * ( 1.0 - x ); 
 			}
@@ -42,6 +43,33 @@ int main(int argc, char* argv[])
 		};
 	};
 
-	RunTasks(1000, taskGenerator);
+	std::cout << "Logistic map" << std::endl;
+	RunTasks(10000, taskGenerator);
+	
+	auto streamTaskGenerator = []()
+	{
+		return []()
+		{
+			std::stringstream ss;
+      ss << 5 << 7 << "Hello" << 4.6 << -0.1;
+			if ( ss.str() == "Nicaragua" ) std::cout << "FAIL" << std::endl;
+		};
+	};
+
+	std::cout << "build string with stringstream" << std::endl;
+	RunTasks(100000, streamTaskGenerator);
+	
+	std::mutex m;
+
+	auto mutexTaskGenerator = [&m]()
+	{
+		return [&m]()
+		{
+			std::lock_guard<std::mutex> l(m);
+		};
+	};
+
+	std::cout << "mutex lock" << std::endl;
+	RunTasks(100000, mutexTaskGenerator);
 }
 
