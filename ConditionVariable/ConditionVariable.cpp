d1aba594f513b9e1db14a54d84651faf03f0f59b
Jonathan Coe <jon@jbcoe.net>
2013-02-07 00:06:33 +0000

    Condition variable toy example

--- ConditionVariable/ConditionVariable.cpp
+++ ConditionVariable/ConditionVariable.cpp
@@ -11,14 +11,17 @@ std::condition_variable data_condition;
 void data_reading()
 {
 	std::string line;
+	std::cout << "Input: (Empty line exits) ";
 	while ( getline(std::cin, line) )
 	{
+		if ( line.empty() )
+			break;
 		std::lock_guard<std::mutex> lg(m);
 		messages.push(line);
 		data_condition.notify_one();
 	}
 }
-                                
+
 void data_printing()
 {
 	while (true)
@@ -36,7 +39,7 @@ int main(int argc, char* argv[])
 {
 	std::thread t_read(data_reading);
 	std::thread t_print(data_printing);
+	t_print.detach();
 	t_read.join();
-	t_print.join();
 }
 
