2a4f3c52b64f91e8db1b470eb72eef508ded2060
Jonathan Coe <jon@jbcoe.net>
2013-03-07 20:05:34 +0000

    When is virtual needed - investigation

---
+++ VirtualFun/VirtualFun.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+struct A 
+{ 
+	void f() { std::cout << "A::f" <<std::endl; } 
+};
+
+struct B : A 
+{ 
+	virtual void f() { std::cout << "B::f" <<std::endl; } 
+};
+
+struct C : B 
+{ 
+	void f() { std::cout << "C::f" <<std::endl; } 
+};
+
+struct D : C 
+{ 
+	void f() { std::cout << "D::f" <<std::endl; } 
+};
+
+int main(int argc, char* argv[])
+{
+	D d;
+
+	A& aRd = d;
+	aRd.f();
+
+	B& bRd = d;
+	bRd.f();
+	
+	C& cRd = d;
+	cRd.f();
+}
+
