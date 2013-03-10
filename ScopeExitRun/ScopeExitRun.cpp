69c1b3b63a96e82ebe67d30dd84f47a7dc3f6f6b
Jonathan Coe <jon@jbcoe.net>
2013-03-10 21:45:05 +0000

    Function pointers can be used as template arguments : demo

--- ScopeExitRun/ScopeExitRun.cpp
+++ ScopeExitRun/ScopeExitRun.cpp
@@ -17,10 +17,16 @@ ScopeExitRunner<F_t> run_on_scope_exit(F_t f)
 	return ScopeExitRunner<F_t>(f);
 }
 
+void f()
+{
+	std::cout << "Can I use a function pointer as a template argument? : Yes " << std::endl;
+}
+
 int main(int argc, char* argv[])
 {
 	auto printHelloWorldOnExit = run_on_scope_exit([](){std::cout << "Hello world" << std::endl;});
 	auto printGoodbyeWorldOnExit = run_on_scope_exit([](){std::cout << "Goodbye cruel world" << std::endl;});
+	auto functionPointerTest = run_on_scope_exit(&f);
 	printGoodbyeWorldOnExit.Release();
 }
 
