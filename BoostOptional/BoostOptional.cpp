f9822768e5e70868064e11e4d42d4d475da2e963
Jonathan Coe <jon@jbcoe.net>
2013-05-07 21:28:32 +0100

    play with optional

---
+++ BoostOptional/BoostOptional.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <boost/optional.hpp>
+
+using namespace boost;
+
+int main(int argc, char* argv[])
+{
+	optional<int> noInt;
+	optional<int> numberFive(5);
+
+	if ( noInt )
+	{
+		std::cout << *noInt << std::endl;
+	}
+	
+	if ( numberFive )
+	{
+		std::cout << *numberFive<< std::endl;
+	}
+}
+
