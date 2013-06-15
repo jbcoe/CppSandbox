b7df25b141de5779ee0407a63752501fe426be7b
Jonathan Coe <jon@jbcoe.net>
2013-06-15 23:04:17 +0100

    Test for initialization of global object - fine under clang and gcc

---
+++ GlobalObjects/GlobalObjects.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+class A
+{
+	public:
+		A(std::string s) : m_name(std::move(s)) {}
+
+		const std::string& GetName() { return m_name; }
+
+	private:
+		std::string m_name;
+};
+
+class B
+{
+	public:
+		static std::unique_ptr<A> g_ptrA;
+};
+
+std::unique_ptr<A> B::g_ptrA = std::unique_ptr<A>(new A("Global A"));
+
+int main(int argc, const char * argv[])
+{
+	std::cout << B::g_ptrA->GetName() << std::endl;
+	return 0;
+}
