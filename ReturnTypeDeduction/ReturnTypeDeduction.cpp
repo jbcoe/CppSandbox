999cdc3be3d6d2c3e2b5c5d973f103992e3ce8b7
Jonathan Coe <jon@jbcoe.net>
2013-06-18 01:06:41 +0100

    C++14 return type deduction supported by gcc4.8

---
+++ ReturnTypeDeduction/ReturnTypeDeduction.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+auto GetName()
+{
+	return "My Name";
+}
+
+int main(int argc, char* argv[])
+{
+	std::cout << GetName() << std::endl;
+}
+
---
+++ compilerSettings_c++14.mk
@@ -0,0 +1,3 @@
+CC = g++-mp-4.8
+OPT=0
+CFLAGS = -Wall -Winline -Wwrite-strings -Wno-unused -O${OPT} -std=c++1y -I../
