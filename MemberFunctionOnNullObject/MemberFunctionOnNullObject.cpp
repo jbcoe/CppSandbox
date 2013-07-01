396534237cec73a6725fef057fdaebed394f47aa
Jonathan Coe <jon@jbcoe.net>
2013-07-01 21:22:24 +0100

    Member function without data access can be called on null object

---
+++ MemberFunctionOnNullObject/MemberFunctionOnNullObject.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class CMyClass
+{
+public:
+  CMyClass(int i) : data{i} {}
+	std::string ClassName() { return "MyClass";}
+	int DataValue() { return data; }
+
+private:
+	int data = 1;
+};
+
+int main(int argc, char* argv[])
+{
+	CMyClass* pObj = nullptr;
+	std::cout << "Member function to get class name (requires no member data) " << pObj->ClassName() << std::endl;
+	//std::cout << "Member function to get data (requires member data) " << pObj->DataValue() << std::endl; // Will crash
+}
+
