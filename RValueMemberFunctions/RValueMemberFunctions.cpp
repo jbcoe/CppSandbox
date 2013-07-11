80591442fb45c0558726b9d63c47c09831303428
Jonathan Coe <jon@jbcoe.net>
2013-07-11 21:52:04 +0100

    rvalue reference to this - demo

---
+++ RValueMemberFunctions/RValueMemberFunctions.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class MyClass
+{
+public:
+  void DoThing() const & { std::cout << "Thing was done as const &" << std::endl; }
+  void DoThing() const && { std::cout << "Thing was done as const &&" << std::endl; }
+
+  void DoThing() & { std::cout << "Thing was done as &" << std::endl; }
+  void DoThing() && { std::cout << "Thing was done as &&" << std::endl; }
+};
+
+int main(int argc, char* argv[])
+{
+	MyClass m;
+	m.DoThing();
+
+	MyClass().DoThing();
+
+	const MyClass& crm = m;
+	crm.DoThing();
+
+	auto returnMyClass = []()->const MyClass{return MyClass();};
+	returnMyClass().DoThing();
+}
+
