013e95e31c251edf5fdbcbee6df0b8a1c01237a7
Jonathan Coe <jon@jbcoe.net>
2013-07-25 20:20:43 +0100

    Logging with streams and thread-safety

--- LoggingWithStreams/LoggingWithStreams.cpp
+++ LoggingWithStreams/LoggingWithStreams.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <mutex>
+#include <thread>
+#include <chrono>
 
 std::ostream* g_logStream = &std::cout;
+
+std::mutex g_logMutex;
+
+class LogLocker
+{
+	public:
+		LogLocker() : l(g_logMutex) {}
+	private:
+	  std::unique_lock<std::mutex> l;
+};
+
+template <typename T>
+std::ostream& operator << (LogLocker&&, T&& t)
+{
+	return *g_logStream << std::forward<T>(t);
+}
 
 enum class LEVEL
 {
@@ -16,20 +35,41 @@ void SetLogLevel(LEVEL logLevel)
 	g_logLevel = logLevel;
 }
 
-#define LOG(level) if(g_logLevel<level); else (*g_logStream)
+#define LOG(level) if(g_logLevel<level); else LogLocker()
 
 int main(int argc, char* argv[])
 {
 	std::vector<LEVEL> logLevels = {LEVEL::INFO,LEVEL::WARNING,LEVEL::ERROR,LEVEL::CRITICAL};
 	for ( auto level : logLevels )
 	{
-    SetLogLevel(level);
+		SetLogLevel(level);
 
 		LOG(LEVEL::INFO) << "It's getting warm" << std::endl;
 		LOG(LEVEL::WARNING) << "It's getting dangerously hot" << std::endl;
 		LOG(LEVEL::ERROR) << "I'm starting to singe a bit" << std::endl;
 		LOG(LEVEL::CRITICAL) << "I am on fire!\n" << std::endl;
 	}
+
+	std::thread t1([]()
+	{
+ 		for (int i=0; i<50; ++i)
+		{ 
+			LOG(LEVEL::CRITICAL) << "Are we locked?" << std::endl;
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		}
+	});
+	
+	std::thread t2([]()
+	{
+ 		for (int i=0; i<50; ++i)
+		{ 
+			LOG(LEVEL::CRITICAL) << "Are we really locked?" << std::endl;
+			std::this_thread::sleep_for(std::chrono::milliseconds(200));
+		}
+	});
+
+	t1.join();
+	t2.join();
 
 	return 0;
 }
