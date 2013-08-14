b3362449b375e4f827dee2a2ddedc295502c29dc
Jonathan Coe <jon@jbcoe.net>
2013-08-14 18:59:32 +0200

    Thread local objects keep live thread count

---
+++ ThreadLocal_RAII/ThreadLocal_RAII.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <thread>
+#include <Common/ScopedThread.h>
+
+std::atomic<int> totalLiveThreadCount(0);
+std::atomic<int> totalCreatedThreadCount(0);
+
+struct ThreadCountInfo
+{
+	ThreadCountInfo() 
+	{
+		++totalLiveThreadCount;
+		++totalCreatedThreadCount;
+	}
+
+	~ThreadCountInfo()
+	{
+		--totalLiveThreadCount;
+	}
+
+	void Init(){}
+};
+
+thread_local ThreadCountInfo tcInfo = ThreadCountInfo{};
+
+std::atomic<bool> keepRunning(true);
+
+int main(int argc, char* argv[])
+{
+	
+	ScopedThread t1([]{tcInfo.Init(); while(keepRunning) std::this_thread::yield();});
+	ScopedThread t2([]{tcInfo.Init(); while(keepRunning) std::this_thread::yield();});
+	ScopedThread t3([]{tcInfo.Init(); while(keepRunning) std::this_thread::yield();});
+	ScopedThread dt1([]{tcInfo.Init();}); // Nothing to do
+
+  while ( totalCreatedThreadCount != 4 && totalLiveThreadCount != 3 ); //Loop
+
+	std::cout << "Total threads spawned: "<< totalCreatedThreadCount << "\nTotal live threads: " << totalLiveThreadCount << std::endl;
+
+  keepRunning = false;
+}
+
