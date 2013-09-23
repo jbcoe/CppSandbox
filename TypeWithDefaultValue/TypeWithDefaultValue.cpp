cbca96932298691eb7651eed46396d83821d1d54
Jonathan Coe <jon@jbcoe.net>
2013-09-23 21:04:30 +0100

    Default value can init ints

---
+++ TypeWithDefaultValue/TypeWithDefaultValue.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+template <typename T, T defaultValue>
+struct TypeWithDefaultValue
+{
+	TypeWithDefaultValue() : m_value(defaultValue) {} 
+
+	template<typename U>
+	TypeWithDefaultValue(U&& u) : m_value(std::forward<U>(u)) {} 
+	
+	template<typename U>
+  TypeWithDefaultValue<T, defaultValue>& operator=(U&& u) 
+	{
+		m_value = std::forward<U>(u);
+	}
+
+	operator T& () { return m_value; }
+
+	operator const T& () const { return m_value; }
+
+	T m_value;
+};
+
+int main(int argc, char* argv[])
+{
+	TypeWithDefaultValue<int,3> myInt3;
+	std::cout << myInt3 << std::endl;
+
+  myInt3 *= 2;
+
+	std::cout << myInt3 << std::endl;
+}
+
