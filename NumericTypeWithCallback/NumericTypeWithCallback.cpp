7740f6325556a4a98aa6411300c8855fb9cb63ea
Jonathan Coe <jon@jbcoe.net>
2013-11-01 22:47:32 +0000

    Numeric types can be streamed

--- NumericTypeWithCallback/NumericTypeWithCallback.cpp
+++ NumericTypeWithCallback/NumericTypeWithCallback.cpp
@@ -3,11 +3,13 @@
 
 #define NUMERIC_CALLBACKS_ENABLED 1
 
+#ifdef NUMERIC_CALLBACKS_ENABLED
 std::function<void(double original, double final)> f_onAssign;
 std::function<void(double first, double second, double result)> f_onAdd;
 std::function<void(double first, double second, double result)> f_onSubtract;
 std::function<void(double first, double second, double result)> f_onMultiply;
 std::function<void(double first, double second, double result)> f_onDivide;
+#endif
 
 template <typename Object_T>
 class Numeric
@@ -60,9 +62,16 @@ public:
 #endif		
 		return Numeric_T(value_/that.value_);
 	}
-	private:
+	
+	double raw_value() const { return value_; }
+
+private:
 		double value_;
 };
+
+template <typename Object_T, typename OS_T>
+OS_T& operator << (OS_T& os, const Numeric<Object_T>& n) { return os << n.raw_value(); } 
+
 
 class MyClass{};
 
@@ -72,11 +81,15 @@ MyNumber operator""_n (long double d) { return MyNumber(static_cast<double>(d));
 
 int main(int argc, char* argv[])
 {
+
+#ifdef NUMERIC_CALLBACKS_ENABLED
   f_onAdd = [](double a, double b, double c) { std::cout << a << " + " << b << " = " << c << std::endl; };
   f_onSubtract = [](double a, double b, double c) { std::cout << a << " - " << b << " = " << c << std::endl; };
   f_onMultiply = [](double a, double b, double c) { std::cout << a << " * " << b << " = " << c << std::endl; };
   f_onDivide = [](double a, double b, double c) { std::cout << a << " / " << b << " = " << c << std::endl; };
+#endif
 
-  auto result = 0.2_n + 0.3_n * 0.3_n - 0.8_n / 2.0_n;
+	auto result = 0.2_n + 0.3_n * 0.3_n - 0.8_n / 2.0_n;
+	std::cout << "result = " << result << std::endl;
 }
 
