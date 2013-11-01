f0f0d43c405eb94eb74c3d5f9bdd631568271fcb
Jonathan Coe <jon@jbcoe.net>
2013-11-01 22:18:11 +0000

    Numeric types with callbacks for logging of floating point ops

---
+++ NumericTypeWithCallback/NumericTypeWithCallback.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <functional>
+
+#define NUMERIC_CALLBACKS_ENABLED 1
+
+std::function<void(double original, double final)> f_onAssign;
+std::function<void(double first, double second, double result)> f_onAdd;
+std::function<void(double first, double second, double result)> f_onSubtract;
+std::function<void(double first, double second, double result)> f_onMultiply;
+std::function<void(double first, double second, double result)> f_onDivide;
+
+template <typename Object_T>
+class Numeric
+{
+public:
+  typedef Numeric<Object_T> Numeric_T;
+
+  Numeric(double value) : value_{value} {}
+
+  Numeric_T operator + ( const Numeric_T& that) const
+	{
+#ifdef NUMERIC_CALLBACKS_ENABLED
+		if( f_onAdd )
+		{
+			f_onAdd(value_, that.value_, value_+that.value_);
+		}
+#endif		
+		return Numeric_T(value_+that.value_);
+	}
+
+  Numeric_T operator - ( const Numeric_T& that) const
+	{
+#ifdef NUMERIC_CALLBACKS_ENABLED
+		if( f_onSubtract )
+		{
+			f_onSubtract(value_, that.value_, value_-that.value_);
+		}
+#endif		
+		return Numeric_T(value_-that.value_);
+	}
+  
+	Numeric_T operator * ( const Numeric_T& that) const
+	{
+#ifdef NUMERIC_CALLBACKS_ENABLED
+		if( f_onMultiply )
+		{
+			f_onMultiply(value_, that.value_, value_*that.value_);
+		}
+#endif		
+		return Numeric_T(value_*that.value_);
+	}
+  
+	Numeric_T operator / ( const Numeric_T& that) const
+	{
+#ifdef NUMERIC_CALLBACKS_ENABLED
+		if( f_onDivide )
+		{
+			f_onDivide(value_, that.value_, value_/that.value_);
+		}
+#endif		
+		return Numeric_T(value_/that.value_);
+	}
+	private:
+		double value_;
+};
+
+class MyClass{};
+
+typedef Numeric<MyClass> MyNumber;
+
+MyNumber operator""_n (long double d) { return MyNumber(static_cast<double>(d)); }
+
+int main(int argc, char* argv[])
+{
+  f_onAdd = [](double a, double b, double c) { std::cout << a << " + " << b << " = " << c << std::endl; };
+  f_onSubtract = [](double a, double b, double c) { std::cout << a << " - " << b << " = " << c << std::endl; };
+  f_onMultiply = [](double a, double b, double c) { std::cout << a << " * " << b << " = " << c << std::endl; };
+  f_onDivide = [](double a, double b, double c) { std::cout << a << " / " << b << " = " << c << std::endl; };
+
+  auto result = 0.2_n + 0.3_n * 0.3_n - 0.8_n / 2.0_n;
+}
+
