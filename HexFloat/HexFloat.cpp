91b52c0b0876ced28e50a168fea0d71b8bdc8174
Jonathan Coe <jon@jbcoe.net>
2013-11-10 20:16:46 +0000

    hexfloat working nicely

---
+++ HexFloat/HexFloat.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <sstream>
+
+int main(int argc, char* argv[])
+{
+	double x = 3.14159;
+	std::cout << x << std::endl;
+
+	std::stringstream ss;
+	ss << std::hexfloat << x;
+	std::cout << ss.str() << std::endl;
+
+	ss >> std::hexfloat >> x;
+	std::cout << x << std::endl;
+}
+
