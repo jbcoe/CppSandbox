96daca4d592bbdc253d1c58db02b25e006bae98b
Jonathan Coe <jon@jbcoe.net>
2013-11-14 19:54:32 +0000

    range-based for and parameter packs

---
+++ VariadicFun/VariadicFun.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+template <typename F, typename... Ts>
+void ApplyToEach(F f, Ts... ts)
+{
+	for ( auto x : {ts...} ) f(x);
+}
+
+int main(int argc, char* argv[])
+{
+	ApplyToEach([](auto x) { std::cout << x << " "; },1,2,3,4,5);
+	std::cout << std::endl;
+}
+
