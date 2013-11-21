6746ea4271290a519346868b0776f3f3023af0ac
Jonathan Coe <jon@jbcoe.net>
2013-11-21 21:37:21 +0000

    reformat

--- HexFloat/HexFloat.cpp
+++ HexFloat/HexFloat.cpp
@@ -3,14 +3,13 @@
 
 int main(int argc, char* argv[])
 {
-	double x = 3.14159;
-	std::cout << x << std::endl;
+  double x = 3.14159;
+  std::cout << x << std::endl;
 
-	std::stringstream ss;
-	ss << std::hexfloat << x;
-	std::cout << ss.str() << std::endl;
+  std::stringstream ss;
+  ss << std::hexfloat << x;
+  std::cout << ss.str() << std::endl;
 
-	ss >> std::hexfloat >> x;
-	std::cout << x << std::endl;
+  ss >> std::hexfloat >> x;
+  std::cout << x << std::endl;
 }
-
--- VariadicFun/VariadicFun.cpp
+++ VariadicFun/VariadicFun.cpp
@@ -3,12 +3,14 @@
 template <typename F, typename... Ts>
 void ApplyToEach(F f, Ts... ts)
 {
-	for ( auto x : {ts...} ) f(x);
+  for (auto x : {ts...})
+    f(x);
 }
 
 int main(int argc, char* argv[])
 {
-	ApplyToEach([](auto x) { std::cout << x << " "; },1,2,3,4,5);
-	std::cout << std::endl;
+  ApplyToEach([](auto x)
+  { std::cout << x << " "; },
+              1, 2, 3, 4, 5);
+  std::cout << std::endl;
 }
-
