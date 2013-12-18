85df29ed5291a4dc8ce58c7f86be63446ee89054
Jonathan Coe <jon@jbcoe.net>
2013-12-18 19:41:47 +0000

    static_assert is better than std::ignore

--- IgnoreForLambdaChecks/IgnoreForLambdaChecks.cpp
+++ IgnoreForLambdaChecks/IgnoreForLambdaChecks.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <functional>
-#include <tuple>
+#include <type_traits>
 
 template <typename T>
 void RunFunction(T t, int i)
 {
-	std::ignore = std::function<std::string(int)>(t);
+	static_assert(std::is_assignable<std::function<std::string(int)>,T>::value, "Function/functor has wrong arguments or return type ");
 	std::cout << t(i) << std::endl; 
 }
 
 int main(int argc, char* argv[])
 {               
 	RunFunction([](int x) { return "std::string (int)"; }, 5);
-	// RunFunction([](int x) { return 4.0; }, 5);  assignment to std::ignore prevents this compiling
+	RunFunction([](int x) { return 4.0; }, 5);  assignment to std::ignore prevents this compiling
 }
 
