44009b9f4e4a2a683e3c4408d8d7fb00f771b013
Jonathan Coe <jon@jbcoe.net>
2013-12-18 23:16:57 +0000

    templated it

--- LargestSumOfConsecutiveElements/LargestSumOfConsecutiveElements.cpp
+++ LargestSumOfConsecutiveElements/LargestSumOfConsecutiveElements.cpp
@@ -4,6 +4,40 @@
 #include <iterator>
 #include <random>
 #include <chrono>
+#include <type_traits>
+
+template<typename Iterator_t>
+std::pair<Iterator_t,	Iterator_t> max_subarray(const Iterator_t begin, const Iterator_t end)
+{
+	typedef typename std::remove_const<typename std::remove_reference<decltype(((Iterator_t*)nullptr)->operator*())>::type>::type Value_t;
+
+	Value_t max_so_far{};
+	Value_t max_ending_here{};
+	auto it_max_begin = begin;
+	auto it_max_begin_temp = begin;
+	auto it_max_end = begin;
+
+  for(auto it=begin, it_end=end; it!=it_end; ++it)
+	{
+		if ( max_ending_here < 0 )
+		{
+			max_ending_here = *it;
+			it_max_begin_temp = it;
+		}
+		else
+		{
+			max_ending_here += *it;
+		}
+    
+		if (max_ending_here>=max_so_far )
+		{
+			max_so_far = max_ending_here;
+			it_max_begin = it_max_begin_temp;
+			it_max_end = std::next(it);
+		}
+	}
+	return {it_max_begin,it_max_end};
+}
 
 int main(int argc, char* argv[])
 {
@@ -23,35 +57,14 @@ int main(int argc, char* argv[])
 	
 	std::copy(begin(myInts), end(myInts), osi);
   std::cout << " [Values]" << std::endl;
-  
-	auto max_so_far = 0;
-	auto max_ending_here = 0;
-	auto it_max_begin = myInts.cbegin();
-	auto it_max_begin_temp = myInts.cbegin();
-	auto it_max_end = myInts.cbegin();
-
-  for(auto it=myInts.cbegin(), it_end=myInts.cend(); it!=it_end; ++it)
-	{
-		if ( max_ending_here < 0 )
-		{
-			max_ending_here = *it;
-			it_max_begin_temp = it;
-		}
-		else
-		{
-			max_ending_here += *it;
-		}
-    
-		if (max_ending_here>=max_so_far )
-		{
-			max_so_far = max_ending_here;
-			it_max_begin = it_max_begin_temp;
-			it_max_end = std::next(it);
-		}
-	}
 	
+	std::vector<int>::const_iterator it_max_begin;
+	std::vector<int>::const_iterator it_max_end;
+
+	std::tie(it_max_begin,it_max_end) = max_subarray(myInts.cbegin(),myInts.cend());
+
 	std::copy(it_max_begin, it_max_end, osi);
-  std::cout << " [Values with largest consecutive sum [" 
-						<< std::distance(myInts.cbegin(),it_max_begin) 
-						<< ':' << std::distance(myInts.cbegin(),it_max_end) << ") ]" << std::endl;
+  //std::cout << " [Values with largest consecutive sum [" 
+	//					<< std::distance(myInts.cbegin(),it_max_begin) 
+	//					<< ':' << std::distance(myInts.cbegin(),it_max_end) << ") ]" << std::endl;
 }
