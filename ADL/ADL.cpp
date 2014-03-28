a7e3ff15e981c77ac57378a2563dc15b576035ad
Jonathan Coe <jon@jbcoe.net>
2014-03-28 22:55:32 +0000

    ADL example

---
+++ ADL/ADL.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+namespace ADL
+{
+	class A {};
+
+	template <typename... Ts> 
+	void foo(const Ts&... )
+	{
+		std::cout << "ADL::foo()" << std::endl;
+	}
+}
+
+namespace OTHER
+{
+	class B {};
+	
+	template <typename... Ts> 
+	void foo(const Ts&...)
+	{
+		std::cout << "OTHER::foo()" << std::endl;
+	}
+}
+
+class C
+{
+	template <typename... Ts> 
+	static void foo(const Ts&...)
+	{
+		std::cout << "C::foo()" << std::endl;
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	ADL::A a;
+	foo(a);
+
+	OTHER::B b;
+	foo(b);
+  
+	ADL::foo(b);
+
+	// foo(a,b); // ambiguous
+	C c;
+	// foo(c); // name not found
+}
+
