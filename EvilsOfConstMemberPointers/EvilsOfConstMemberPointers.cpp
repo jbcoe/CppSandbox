362fc9db3bb260c0f874ece057c175fe389870c2
Jonathan Coe <jon@jbcoe.net>
2014-04-08 17:49:19 +0100

    Evil member pointer

---
+++ EvilsOfConstMemberPointers/EvilsOfConstMemberPointers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+struct Data
+{
+		void mutateMe() { x = true; }
+
+		bool x=false;
+};
+
+
+struct DataObserver
+{
+	DataObserver(Data& d_) : d(&d_) {}
+
+	void ThreadSafeConstMethod() const { d->mutateMe(); }
+
+	Data* d;
+};
+
+int main()
+{
+	Data d;
+	const DataObserver o(d);
+	
+	std::cout << std::boolalpha << d.x << std::endl;
+	
+	o.ThreadSafeConstMethod();
+	
+	std::cout << std::boolalpha << d.x << std::endl;
+}
