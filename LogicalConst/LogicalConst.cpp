46edb88d7a349f31ad479bbd50d33320112bdeec
Jonathan Coe <jon@jbcoe.net>
2014-04-20 17:52:34 +0100

    reformat

--- LogicalConst/LogicalConst.cpp
+++ LogicalConst/LogicalConst.cpp
@@ -2,56 +2,155 @@
 #include <memory>
 
 template <typename T>
-class LogicalConst
+class logical_const
 {
 public:
-
   typedef std::remove_reference_t<decltype(*std::declval<T>())> Value_T;
 
-  LogicalConst(T&& t_) : t(std::move(t_)) {}
-  LogicalConst(const T& t_) : t(t_) {}
+  ~logical_const()
+  {
+  }
+
+  logical_const()
+  {
+  }
+
+  logical_const(const T& t_) : t{t_}
+  {
+  }
+
+  logical_const(T&& t_) : t{std::move(t_)}
+  {
+  }
+
+  logical_const(const logical_const<T>& pt_) : t{pt_.t}
+  {
+  }
+
+  logical_const(logical_const<T>&& pt_) : t{std::move(pt_.t)}
+  {
+  }
+
+  logical_const<T>& operator=(const T& t_)
+  {
+    t = t_;
+    return *this;
+  }
+
+  logical_const<T>& operator=(T&& t_)
+  {
+    t = std::move(t_);
+    return *this;
+  }
+
+  logical_const<T>& operator=(const logical_const<T>& pt_)
+  {
+    t = pt_.t;
+    return *this;
+  }
+
+  logical_const<T>& operator=(logical_const<T>&& pt_)
+  {
+    t = std::move(pt_.t);
+    return *this;
+  }
 
   Value_T* operator->()
-	{
-  	return &*t;
-	}
+  {
+    return &*t;
+  }
 
   const Value_T* operator->() const
-	{
-  	return &*t;
-	}
+  {
+    return &*t;
+  }
+
+  Value_T& operator*()
+  {
+    return *t;
+  }
+
+  const Value_T& operator*() const
+  {
+    return *t;
+  }
+
+  Value_T* get()
+  {
+    return t.get();
+  }
+
+  const Value_T* get() const
+  {
+    return t.get();
+  }
+
+  operator const T&() const
+  {
+    return t;
+  }
+
+  operator T&()
+  {
+    return t;
+  }
 
 private:
-	T t;
+  T t;
 };
+
+template <typename T, typename U>
+bool operator==(const logical_const<T>& pt1, const logical_const<U>& pt2)
+{
+  return pt1.t == pt2.t;
+}
+
+template <typename T, typename U>
+bool operator!=(const logical_const<T>& pt1, const logical_const<U>& pt2)
+{
+  return pt1.t != pt2.t;
+}
+
+template <typename T, typename U>
+void swap(logical_const<T>& pt1, logical_const<U>& pt2)
+{
+  swap(pt1.t, pt2.t);
+}
 
 struct A
 {
-	void bar() const { std::cout << "bar (const)" << std::endl; }
-	void bar() { std::cout << "bar (non-const)" << std::endl; }
+  void bar() const
+  {
+    std::cout << "bar (const)" << std::endl;
+  }
+  void bar()
+  {
+    std::cout << "bar (non-const)" << std::endl;
+  }
 };
 
 struct B
 {
-	B() : m_ptrA(std::make_unique<A>()) {} 
+  B() : m_ptrA(std::make_unique<A>())
+  {
+  }
 
   void operator()()
-	{
-		m_ptrA->bar();
-	}
+  {
+    m_ptrA->bar();
+  }
 
   void operator()() const
-	{
-		m_ptrA->bar();
-	}
-	
-	//LogicalConst<std::unique_ptr<A>> m_ptrA;
-	std::unique_ptr<A> m_ptrA;
+  {
+    m_ptrA->bar();
+  }
+
+  logical_const<std::unique_ptr<A>> m_ptrA;
+  // std::unique_ptr<A> m_ptrA;
 };
 
 int main(int argc, char* argv[])
 {
-	const B b;
-	b();
+  const B b;
+  b();
 }
-
