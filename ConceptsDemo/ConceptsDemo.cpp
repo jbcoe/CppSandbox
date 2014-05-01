9e64fb9c3789977c68ee1b6eb0d9d5e8cf4962db
Jonathan Coe <jon@jbcoe.net>
2014-05-01 21:06:37 +0100

    Use concepts from origin library

--- ConceptsDemo/ConceptsDemo.cpp
+++ ConceptsDemo/ConceptsDemo.cpp
@@ -1,6 +1,9 @@
 // NO CLANG FORMAT (concepts not yet supported)
 #include <iostream>
 #include <type_traits>
+#include "origin/core/Concepts.hpp"
+
+using namespace origin;
 
 template <typename T>
 concept bool Incrementable()
@@ -12,6 +15,10 @@ concept bool Incrementable()
 }
 
 struct MyClass {};
+struct MyNonCopyableClass
+{
+	MyNonCopyableClass(const MyNonCopyableClass&) = delete;
+};
 
 struct MyIncrementableClass
 {
@@ -23,12 +30,18 @@ void foo(Incrementable& i)
 	std::cout << "foo" << std::endl;
 }
 
+#define print_concept(x,y) std::cout << std::boolalpha << #x << " " << #y << " " << x<y>() << std::endl 
+
 int main(int argc, char* argv[])
 {
 	MyClass m;
-	foo(m); // will not compile
+	//foo(m); // will not compile
 
 	MyIncrementableClass im;
 	foo(im);
+
+	print_concept(Copyable, MyClass);
+	print_concept(Copyable, MyNonCopyableClass);
 }
+
 
--- compilerSettings_g++concepts.mk
+++ compilerSettings_g++concepts.mk
@@ -1,4 +1,4 @@
 CC = g++-concepts
 OPT=0
-CFLAGS = -Wall -Winline -Wwrite-strings -Wno-unused -O${OPT} -std=c++11 -I../
+CFLAGS = -Wall -Winline -Wwrite-strings -Wno-unused -O${OPT} -std=c++11 -I../ -I../ConceptsOrigin/
 LDFLAGS = 
