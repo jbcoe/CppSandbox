98c5f64794342e46489170b214efb9b3b0fb361d
Jonathan Coe <jon@jbcoe.net>
2014-05-21 23:19:33 +0100

    reformat script leaves ConceptsOrigin be

--- reformat.sh
+++ reformat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-clang-format -i $(find . -iname "*.cpp" -exec grep -L -e "NO CLANG FORMAT" {} \; )
-clang-format -i $(find . -iname "*.hpp" -exec grep -L -e "NO CLANG FORMAT" {} \; )
-clang-format -i $(find . -iname "*.h" -exec grep -L -e "NO CLANG FORMAT" {} \; )
+clang-format -i $(find . ! -path "./ConceptsOrigin*" -iname "*.cpp" -exec grep -L -e "NO CLANG FORMAT" {} \; )
+clang-format -i $(find . ! -path "./ConceptsOrigin*" -iname "*.h" -exec grep -L -e "NO CLANG FORMAT" {} \; )
+clang-format -i $(find . ! -path "./ConceptsOrigin*" -iname "*.hpp" -exec grep -L -e "NO CLANG FORMAT" {} \; )
 
