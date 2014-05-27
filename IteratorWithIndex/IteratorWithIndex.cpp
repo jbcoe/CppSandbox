52bf1a973fd5bd56e23163730b99054592689793
Jonathan Coe <jon@jbcoe.net>
2014-05-27 22:20:13 +0100

    Iterator with index uses reference wrapper

--- IteratorWithIndex/IteratorWithIndex.cpp
+++ IteratorWithIndex/IteratorWithIndex.cpp
@@ -9,7 +9,7 @@ private:
   size_t m_i;
 
 public:
-  IteratorWithIndex(Iterator_T it, size_t i = 0) : m_it(it), m_i(i) {}
+  IteratorWithIndex(Iterator_T it, size_t i = 0) : m_it(std::move(it)), m_i(i) {}
 
   IteratorWithIndex& operator++()
   {
@@ -19,8 +19,45 @@ public:
   }
 
   bool operator==(const IteratorWithIndex& i) const { return m_it == i.m_it; }
+  bool operator==(const Iterator_T& i) const { return m_it == i; }
+  
+	bool operator!=(const IteratorWithIndex& i) const { return m_it != i.m_it; }
+	bool operator!=(const Iterator_T& i) const { return m_it != i; }
 
-  auto operator*() const { return std::make_pair(*m_it, m_i); }
+  auto index() const { return m_i; }
+
+  auto operator*() const { return std::make_pair(m_i,std::cref(*m_it)); }
+  auto operator*() { return std::make_pair(m_i,std::ref(*m_it)); }
 };
 
-int main(int argc, char* argv[]) {}
+template<typename Iterator_T>
+class IndexedRange
+{
+	IteratorWithIndex<Iterator_T> begin_;
+	IteratorWithIndex<Iterator_T> end_;
+
+public:
+	IndexedRange(Iterator_T begin, Iterator_T end) : begin_(std::move(begin)), end_(std::move(end)) {} 
+
+	auto begin() { return begin_; }
+	auto end() { return end_; }
+};
+
+template<typename T>
+auto make_indexed_range(T& t)
+{
+	typedef decltype(t.begin()) Iterator_T; 
+	return IndexedRange<Iterator_T>(t.begin(),t.end());
+}
+
+int main(int argc, char* argv[]) 
+{
+	std::vector<std::string> v = {"I","II","III","IV","V"};
+
+  auto iv = make_indexed_range(v); 
+
+	for(auto x: iv)
+	{
+		std::cout << x.first << " : " << x.second << "\n";
+	}
+}
