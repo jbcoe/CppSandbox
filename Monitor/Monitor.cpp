4bca39519f1e75ff3b4a1b37b34e610eb72e5a20
Jonathan Coe <jon@jbcoe.net>
2014-06-10 00:09:04 +0100

    Tweaks after discussion with A Sawyer and A Williams @ BSI

--- Monitor/Monitor.cpp
+++ Monitor/Monitor.cpp
@@ -1,75 +1,88 @@
 // Based on Herb Sutter's talk at C++ and beyond 2012
-// He made operator() const which I think is incorrect
 
 #include <iostream>
 #include <algorithm>
 #include <string>
 #include <thread>
+#include <sstream>
 #include <vector>
 
 template <typename T>
 class Monitor
 {
-  T t;
-  std::mutex m;
+  T t_;
+  mutable std::mutex m_;
+
+  operator const T&() const { return t_; }
+
+  operator T&() { return t_; }
 
 public:
-  Monitor(T _t) : t(_t) {}
+
+  Monitor()=default;
+  Monitor(const Monitor&)=delete;
+  Monitor(Monitor&&)=delete;
+  Monitor& operator=(const Monitor&)=delete;
+  Monitor& operator=(Monitor&&)=delete;
 
   template <typename F>
   auto operator()(F f)
   {
-    std::lock_guard<std::mutex> l(m);
-    return f(t);
+    std::lock_guard<std::mutex> l(m_);
+    return f(static_cast<T&>(*this));
   }
 
   template <typename F>
   auto operator()(F f) const
   {
-    std::lock_guard<std::mutex> l(m);
-    return f(const_cast<const T&>(t));
+    std::lock_guard<std::mutex> l(m_);
+    return f(static_cast<const T&>(*this));
   }
 };
 
 
 int main(int argc, char* argv[])
 {
-  Monitor<std::ostream&> async_ios(std::ref(std::cout));
+  Monitor<std::stringstream> ss;
 
   std::vector<std::thread> threads;
 
-  threads.emplace_back([&]()
+  
+	threads.emplace_back([&]()
                        {
-                         async_ios([](auto& io)
+                         ss([](auto& ss)
                                    {
-                                     io << "Hello 1 : "
+                                     ss << "Hello 1 : "
                                         << std::this_thread::get_id()
                                         << std::endl;
                                    });
                        });
-  threads.emplace_back([&]()
+  
+	threads.emplace_back([&]()
                        {
-                         async_ios([](auto& io)
+                         ss([](auto& ss)
                                    {
-                                     io << "Hello 2 : "
+                                     ss << "Hello 2 : "
                                         << std::this_thread::get_id()
                                         << std::endl;
                                    });
                        });
-  threads.emplace_back([&]()
+  
+	threads.emplace_back([&]()
                        {
-                         async_ios([](auto& io)
+                         ss([](auto& ss)
                                    {
-                                     io << "Hello 3 : "
+                                     ss << "Hello 3 : "
                                         << std::this_thread::get_id()
                                         << std::endl;
                                    });
                        });
-  threads.emplace_back([&]()
+  
+	threads.emplace_back([&]()
                        {
-                         async_ios([](auto& io)
+                         ss([](auto& ss)
                                    {
-                                     io << "Hello 4 : "
+                                     ss << "Hello 4 : "
                                         << std::this_thread::get_id()
                                         << std::endl;
                                    });
@@ -79,4 +92,6 @@ int main(int argc, char* argv[])
   {
     t.join();
   }
+
+	ss([](auto& ss){ std::cout << ss.str() << std::endl; });
 }
