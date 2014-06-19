55698ccbac7e5cb55dc10969d39e299850d1e9a6
Jonathan Coe <jon@jbcoe.net>
2014-06-19 09:01:01 +0200

    modified clang compiler settings to use c++14

--- compilerSettings_c++14.mk
+++
@@ -1,3 +0,0 @@
-CC = clang++-mp-3.5
-OPT=0
-CFLAGS = -Wall -Winline -Wwrite-strings -Wno-unused -O${OPT} -std=c++1y -I../
--- compilerSettings_clang3.5.mk
+++ compilerSettings_clang3.5.mk
@@ -1,4 +1,4 @@
 CC = clang++-mp-3.5
 OPT=0
-CFLAGS = -Wall -Winline -Wwrite-strings -Wno-unused -O${OPT} -std=c++11 -stdlib=libc++ -I../
+CFLAGS = -Wall -Winline -Wwrite-strings -Wno-unused -O${OPT} -std=c++1y -stdlib=libc++ -I../
 LDFLAGS = -stdlib=libc++
