694bdca779d916b8e6cda6f4ae7e3b4f9889af6a
Jonathan Coe <jon@jbcoe.net>
2014-10-27 14:58:28 +0000

    Based on Anthony Williams Synchronized Value proposal

---
+++ SynchronizedValue/SynchronizedValue.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <thread>
+
+template <typename T>
+class SynchronizedValue
+{
+  T t_;
+  mutable std::mutex m_;
+
+public:
+  class Guard
+  {
+    const T* t_;
+    std::unique_lock<std::mutex> l_;
+
+  public:
+    Guard(const T& t, std::mutex& m) : t_(&t), l_(m) {}
+
+    const T* operator->() const { return t_; }
+  };
+
+  class EditGuard
+  {
+    T* t_;
+    std::unique_lock<std::mutex> l_;
+
+  public:
+    EditGuard(const T& t, std::mutex& m) : t_(&t), l_(m) {}
+
+    const T* operator->() const { return t_; }
+
+    T* operator->() { return t_; }
+  };
+  
+  SynchronizedValue() : t_() {}
+
+  SynchronizedValue(const T& t) : t_(t) {}
+
+  SynchronizedValue(T&& t) : t_(std::move(t)) {}
+
+  Guard Access() const { return Guard(t_, m_); }
+};
+
+class MyClass
+{
+public:
+  void Greet(const char* s) const
+  {
+    std::cout << '"' << greeting_ << '"' << " from " << s << std::endl;
+  }
+
+  void SetGreeting(const char* greeting)
+  {
+    greeting_ = greeting;
+  }
+
+  std::string greeting_ = "Hello";
+};
+
+int main(int argc, char* argv[])
+{
+  SynchronizedValue<MyClass> s;
+  {
+    auto g = s.Access();
+
+    g->Greet("Main thread");
+  }
+
+  std::vector<std::thread> ts;
+
+  for (size_t i = 0; i < 5; ++i)
+  {
+    ts.emplace_back([&,i]()
+                    {
+                      auto g = s.Access();
+                      std::ostringstream ss;
+                      ss << "Spawned thread " << i;
+                      g->Greet(ss.str().c_str());
+                    });
+  }
+
+  for(auto& t : ts ) 
+  {
+    t.join();
+  }
+}
+
