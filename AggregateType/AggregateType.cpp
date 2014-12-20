3a71c26bc9c7c8622ef61ff2e0d1e25d34d0b437
Jonathan Coe <jon@jbcoe.net>
2014-12-20 23:53:52 +0000

    Aggregate type works for non-variadic type list

--- AggregateType/AggregateType.cpp
+++ AggregateType/AggregateType.cpp
@@ -1,46 +1,69 @@
 #include <iostream>
 #include <memory>
 
-template <typename T, typename... Ts>
-class AggregateImpl : public AggregateImpl<Ts...>
+template <typename T1, typename T2, typename T3>
+class Aggregate
 {
 public:
   template <typename U>
-  AggregateImpl(std::shared_ptr<U> u)
-      : AggregateImpl<Ts...>(std::move(u))
+  Aggregate(std::shared_ptr<U> u) 
   {
+      impl_ = std::make_shared<InnerImpl<U>>(std::move(u));
   }
 
-  struct Inner : public AggregateImpl<Ts...>::Inner
+  template <typename V>
+  V& As()
   {
-    virtual operator T&() = 0;
-  };
-};
-
-template <typename T>
-class AggregateImpl<T>
-{
-public:
-  template <typename U>
-  AggregateImpl(std::shared_ptr<U> u)
-  {
+    return static_cast<V&>(*impl_);
   }
+
+private:
 
   struct Inner
   {
-    virtual operator T&() = 0;
+    virtual operator T1& () = 0;  
+    virtual operator T2& () = 0;  
+    virtual operator T3& () = 0;  
   };
 
+  std::shared_ptr<Inner> impl_;
+  
+  template <typename U>
+  struct InnerImpl : Inner
+  {
+    InnerImpl(std::shared_ptr<U> u) : u_(std::move(u)) {}
+
+    operator T1& () override { return *u_; }   
+    operator T2& () override { return *u_; }  
+    operator T3& () override { return *u_; }  
+    
+    std::shared_ptr<U> u_;
+  };
 };
 
-class Cat{}; class Pig{}; class Dog{};
+struct Cat
+{
+  const char* MakeNoise() const { return "Meeow"; }
+}; 
+
+struct Pig
+{
+  const char* MakeNoise() const { return "Oink"; }
+}; 
+
+struct Dog
+{
+  const char* MakeNoise() const { return "Woof"; }
+};
 
 struct Chimera : Cat, Pig, Dog {};
 
 int main(int argc, char* argv[]) {
 
-  AggregateImpl<Cat,Pig,Dog> agg(std::make_shared<Chimera>());
+  Aggregate<Cat,Pig,Dog> agg(std::make_shared<Chimera>());
+
+  std::cout << agg.As<Cat>().MakeNoise() << '\n';
+  std::cout << agg.As<Pig>().MakeNoise() << '\n';
+  std::cout << agg.As<Dog>().MakeNoise() << '\n';
 }
-
-
 
