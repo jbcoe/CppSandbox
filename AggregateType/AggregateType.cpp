d45b34e986c38439dbe50f87f7836fe02965fbe7
Jonathan Coe <jon@jbcoe.net>
2014-12-21 14:08:04 +0000

    Can now be interpreted as a shared_ptr

--- AggregateType/AggregateType.cpp
+++ AggregateType/AggregateType.cpp
@@ -17,6 +17,12 @@ public:
     return static_cast<V&>(*impl_);
   }
 
+  template <typename V>
+  std::shared_ptr<V> AsPtr()
+  {
+    return static_cast<std::shared_ptr<V>>(*impl_);
+  }
+
 private:
 
   struct Inner
@@ -24,6 +30,10 @@ private:
     virtual operator T1& () = 0;  
     virtual operator T2& () = 0;  
     virtual operator T3& () = 0;  
+    
+    virtual operator std::shared_ptr<T1> () = 0;
+    virtual operator std::shared_ptr<T2> () = 0;
+    virtual operator std::shared_ptr<T3> () = 0;
   };
 
   std::shared_ptr<Inner> impl_;
@@ -36,6 +46,10 @@ private:
     operator T1& () override { return *u_; }   
     operator T2& () override { return *u_; }  
     operator T3& () override { return *u_; }  
+    
+    operator std::shared_ptr<T1> () override { return u_; }   
+    operator std::shared_ptr<T2> () override { return u_; }  
+    operator std::shared_ptr<T3> () override { return u_; }  
     
     std::shared_ptr<U> u_;
   };
@@ -62,8 +76,8 @@ int main(int argc, char* argv[]) {
 
   Aggregate<Cat,Pig,Dog> agg(std::make_shared<Chimera>());
 
-  std::cout << agg.As<Cat>().MakeNoise() << '\n';
-  std::cout << agg.As<Pig>().MakeNoise() << '\n';
-  std::cout << agg.As<Dog>().MakeNoise() << '\n';
+  std::cout << agg.AsPtr<Cat>()->MakeNoise() << '\n';
+  std::cout << agg.AsPtr<Pig>()->MakeNoise() << '\n';
+  std::cout << agg.AsPtr<Dog>()->MakeNoise() << '\n';
 }
 
