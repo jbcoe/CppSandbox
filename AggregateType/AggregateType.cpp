8c22ef8693a50765bc1ae97f585b6f2821cf245d
Jonathan Coe <jon@jbcoe.net>
2014-12-21 21:38:23 +0000

    Added const

--- AggregateType/AggregateType.cpp
+++ AggregateType/AggregateType.cpp
@@ -6,52 +6,52 @@ class Aggregate
 {
 public:
   template <typename U>
-  Aggregate(std::shared_ptr<U> u) 
+  Aggregate(std::shared_ptr<const U> u) 
   {
-      impl_ = std::make_shared<InnerImpl<U>>(std::move(u));
+      impl_ = std::make_shared<const InnerImpl<U>>(std::move(u));
   }
 
   template <typename V>
-  V& As()
+  V& As() const
   {
     return static_cast<V&>(*impl_);
   }
 
   template <typename V>
-  std::shared_ptr<V> AsPtr()
+  std::shared_ptr<const V> AsPtr() const
   {
-    return static_cast<std::shared_ptr<V>>(*impl_);
+    return static_cast<std::shared_ptr<const V>>(*impl_);
   }
 
 private:
 
   struct Inner
   {
-    virtual operator T1& () = 0;  
-    virtual operator T2& () = 0;  
-    virtual operator T3& () = 0;  
+    virtual operator const T1& () const = 0;  
+    virtual operator const T2& () const = 0;  
+    virtual operator const T3& () const = 0;  
     
-    virtual operator std::shared_ptr<T1> () = 0;
-    virtual operator std::shared_ptr<T2> () = 0;
-    virtual operator std::shared_ptr<T3> () = 0;
+    virtual operator std::shared_ptr<const T1> () const = 0;
+    virtual operator std::shared_ptr<const T2> () const = 0;
+    virtual operator std::shared_ptr<const T3> () const = 0;
   };
 
-  std::shared_ptr<Inner> impl_;
+  std::shared_ptr<const Inner> impl_;
   
   template <typename U>
   struct InnerImpl : Inner
   {
-    InnerImpl(std::shared_ptr<U> u) : u_(std::move(u)) {}
+    InnerImpl(std::shared_ptr<const U> u) : u_(std::move(u)) {}
 
-    operator T1& () override { return *u_; }   
-    operator T2& () override { return *u_; }  
-    operator T3& () override { return *u_; }  
+    operator const T1& () const override { return *u_; }   
+    operator const T2& () const override { return *u_; }  
+    operator const T3& () const override { return *u_; }  
     
-    operator std::shared_ptr<T1> () override { return u_; }   
-    operator std::shared_ptr<T2> () override { return u_; }  
-    operator std::shared_ptr<T3> () override { return u_; }  
+    operator std::shared_ptr<const T1> () const override { return u_; }   
+    operator std::shared_ptr<const T2> () const override { return u_; }  
+    operator std::shared_ptr<const T3> () const override { return u_; }  
     
-    std::shared_ptr<U> u_;
+    std::shared_ptr<const U> u_;
   };
 };
 
@@ -74,7 +74,7 @@ struct Chimera : Cat, Pig, Dog {};
 
 int main(int argc, char* argv[]) {
 
-  Aggregate<Cat,Pig,Dog> agg(std::make_shared<Chimera>());
+  Aggregate<Cat,Pig,Dog> agg(std::make_shared<const Chimera>());
 
   std::cout << agg.AsPtr<Cat>()->MakeNoise() << '\n';
   std::cout << agg.AsPtr<Pig>()->MakeNoise() << '\n';
