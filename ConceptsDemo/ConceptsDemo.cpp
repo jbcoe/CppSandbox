fccdfdc9080a36eb1bd3d63af2b51ab17669b556
Jonathan Coe <jon@jbcoe.net>
2015-01-07 12:54:31 +0000

    Added Cloneable concept for Etuka

--- ConceptsDemo/ConceptsDemo.cpp
+++ ConceptsDemo/ConceptsDemo.cpp
@@ -1,23 +1,26 @@
 // NO CLANG FORMAT (concepts not yet supported)
 #include <iostream>
 #include <type_traits>
+#include <memory>
 #include "origin/core/Concepts.hpp"
 #include "origin/sequence/Concepts.hpp"
 
 using namespace origin;
 
-/*
 template <typename T>
-concept bool Advanceable()
+concept bool Cloneable()
 {
-  return requires (T t) 
-  { 
-    ++t; 
+  return requires(T* t)
+  {
+    { t->Clone() } -> std::unique_ptr<T>;
   };
 }
-*/
 
-struct MyClass {};
+struct MyClass 
+{
+  std::unique_ptr<MyClass> Clone() { return std::unique_ptr<MyClass>(new MyClass()); }
+};
+
 struct MyNonCopyableClass
 {
   MyNonCopyableClass(const MyNonCopyableClass&) = delete;
@@ -51,6 +54,8 @@ int main(int argc, char* argv[])
 
   print_concept(Copyable, MyClass);
   print_concept(Copyable, MyNonCopyableClass);
+
+  std::cout << Cloneable<MyClass>() << std::endl;
 }
 
 
