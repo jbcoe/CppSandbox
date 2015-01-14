936a3e1532b89d458d04b227f0a6fd43771ff336
Jonathan Coe <jon@jbcoe.net>
2015-01-14 22:05:35 +0000

    index map ok

--- Maps/Maps.cpp
+++ Maps/Maps.cpp
@@ -5,6 +5,56 @@
 #include <vector>
 #include <deque>
 
+template<typename K,typename V>
+class IndexedKeyMap
+{
+  std::vector<K> keys_;
+  std::vector<V> values_;
+
+public:
+  
+  const V* find(const K& k) const
+  {
+    auto find_it = std::lower_bound(keys_.begin(), keys_.end(), k, [](const auto& k_, const auto& k)
+        {
+          return k_ < k;
+        });
+  
+    if (find_it == keys_.end() || *find_it != k)
+    {
+      return nullptr;
+    }
+    auto index = std::distance(keys_.begin(), find_it);
+    return &values_[index];
+  }
+
+  bool insert(const std::pair<K,V>& kv)
+  {
+    auto find_it = std::lower_bound(keys_.begin(), keys_.end(), kv.first,
+                                        [](const auto& kv, const auto& k)
+                                        {
+                                          return kv < k;
+                                        });
+    if (find_it == keys_.end())
+    {
+      keys_.push_back(kv.first);
+      values_.push_back(kv.second);
+      return true;
+    }                                      
+    if (*find_it == kv.first)
+    {
+      return false;
+    }
+    
+    auto index = std::distance(keys_.begin(), find_it);
+    
+    keys_.insert(keys_.begin()+index, kv.first);
+    values_.insert(values_.begin()+index, kv.second);
+    
+    return true;
+  }
+};
+
 template <typename K, typename V>
 class FlatMap
 {
@@ -62,6 +112,8 @@ int main(int argc, char* argv[])
   const int BIG_NUMBER = 1E6;
 
   FlatMap<int, double> flatMappedRandomNumbers;
+  std::map<int, double> stdMappedRandomNumbers;
+  IndexedKeyMap<int, double> indexedKeyRandomNumbers;
 
   std::uniform_real_distribution<double> value_distribution(-1, 1);
   std::uniform_int_distribution<int> key_distribution(0, BIG_NUMBER / 100);
@@ -88,7 +140,6 @@ int main(int argc, char* argv[])
   }
 
   engine.seed(0);
-  std::map<int, double> stdMappedRandomNumbers;
   {
     auto t = make_timer("std::map");
     for (int i = 0; i < BIG_NUMBER; ++i)
@@ -98,6 +149,16 @@ int main(int argc, char* argv[])
     }
   }
 
+  engine.seed(0);
+  {
+    auto t = make_timer("index map");
+    for (int i = 0; i < BIG_NUMBER; ++i)
+    {
+      indexedKeyRandomNumbers.insert(
+          std::make_pair(key_generator(), value_generator()));
+    }
+  }
+  
   std::cout << "\nLookup\n";
   engine.seed(0);
   auto hits_0 = 0;
@@ -126,6 +187,19 @@ int main(int argc, char* argv[])
     }
   }
 
-  return hits_0 == hits_1 ? 0 : -1;
+  engine.seed(0);
+  auto hits_2 = 0;
+  {
+    auto t = make_timer("index map");
+    for (int i = 0; i < BIG_NUMBER; ++i)
+    {
+      if (indexedKeyRandomNumbers.find(key_generator()))
+      {
+        ++hits_2;
+      }
+    }
+  }
+  
+  return ( hits_0 == hits_1 && hits_1 == hits_2 ) ? 0 : -1;
 }
 
