0909dfd67c44861ce4d041b99c359a27c8649b3a
Jonathan Coe <jon@jbcoe.net>
2015-05-27 23:58:44 +0100

    format

--- RangesV3Demo_View/RangesV3Demo_View.cpp
+++ RangesV3Demo_View/RangesV3Demo_View.cpp
@@ -6,9 +6,16 @@
 using namespace ranges;
 
 int main(int argc, char* argv[])
-{                          
-  std::vector<int> xs = {0,1,2,3,4,5,6,7,8,9};
-  for ( auto x : xs | view::remove_if([](int x){return x%2==0;}) | view::transform([](int x){return x*x;})) std::cout << x << ' ';
+{
+  std::vector<int> xs = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  for (auto x : xs | view::remove_if([](int x)
+                                     {
+                                       return x % 2 == 0;
+                                     }) |
+                    view::transform([](int x)
+                                    {
+                                      return x * x;
+                                    }))
+    std::cout << x << ' ';
   std::cout << std::endl;
 }
-
