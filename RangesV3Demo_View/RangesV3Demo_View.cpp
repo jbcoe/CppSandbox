b70bc12c7f0186eae7bee10fc822c90f96fa7f58
Jonathan Coe <jon@jbcoe.net>
2015-05-27 23:03:06 +0100

    ranges view demo

---
+++ RangesV3Demo_View/RangesV3Demo_View.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <range/v3/core.hpp>
+#include <range/v3/view.hpp>
+
+using namespace ranges;
+
+int main(int argc, char* argv[])
+{                          
+  std::vector<int> xs = {0,1,2,3,4,5,6,7,8,9};
+  for ( auto x : xs | view::remove_if([](int x){return x%2==0;})) std::cout << x << ' ';
+  std::cout << std::endl;
+}
+
