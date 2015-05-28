c182902db9bfafe5c6d5fb9f9d8c90dcb77cc335
Jonathan Coe <jon@jbcoe.net>
2015-05-28 19:31:30 +0100

    nicer demo with more ranges::view use

--- RangesV3Demo_View/RangesV3Demo_View.cpp
+++ RangesV3Demo_View/RangesV3Demo_View.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
-#include <vector>
 #include <range/v3/core.hpp>
 #include <range/v3/view.hpp>
+#include <range/v3/algorithm.hpp>
 
 using namespace ranges;
 
+
 int main(int argc, char* argv[])
 {
-  std::vector<int> xs = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  for (auto x : xs | view::remove_if([](auto x)
-                                     {
-                                       return x % 2 == 0;
-                                     }) |
-                    view::transform([](auto x)
-                                    {
-                                      return x * x;
-                                    }))
-    std::cout << x << ' ';
+  for_each(view::ints(1) | view::take(10) 
+                         | view::remove_if([](auto x) { return x % 2 == 0; }) 
+                         | view::transform([](auto x) { return x * x; }),
+           [](auto x) { std::cout << x << ' '; });
+
   std::cout << std::endl;
 }
