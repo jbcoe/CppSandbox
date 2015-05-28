54d772b9f3a1157d0ae60c15da142b0339fd328d
Jonathan Coe <jon@jbcoe.net>
2015-05-28 23:39:31 +0100

    use gcc 5

--- compilerSettings_gcc.mk
+++ compilerSettings_gcc.mk
@@ -1,8 +1,8 @@
-CC=gcc-mp-4.9
-CXX=g++-mp-4.9
+CC=gcc-mp-5
+CXX=g++-mp-5
 RM=rm -f
 OPT=0
-CPPFLAGS= -Wall -Winline -Wwrite-strings -Wno-unused -O$(OPT) -std=c++1y -I../
+CPPFLAGS= -Wall -Winline -Wwrite-strings -Wno-unused -O$(OPT) -std=c++14 -I../
 LDFLAGS=
 LDLIBS=
  
