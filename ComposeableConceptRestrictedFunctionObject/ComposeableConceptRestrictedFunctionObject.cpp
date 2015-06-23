17e63f33ba8e41faf83de8a259a7f97e5774298a
Jonathan Coe <jon@jbcoe.net>
2015-06-23 18:20:22 +0100

    Composeable function object can be constrained with concepts

---
+++ ComposeableConceptRestrictedFunctionObject/ComposeableConceptRestrictedFunctionObject.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <boost/variant.hpp>
+
+using std::nullptr_t;
+using std::pair;
+using std::make_pair;
+using std::move;
+using std::cout;
+
+
+template <typename F, typename T>
+concept bool Invokeable()
+{
+  return requires(const F& f, const T& t)
+  {
+    { f(t) } -> void;
+  };
+}
+
+template <typename F, typename BaseInner, typename ArgsT>
+struct ComposeVariantVisitor
+{
+  struct Inner : BaseInner
+  {
+    Inner(ArgsT&& a) : BaseInner(move(a.second)), f_(move(a.first)) {}
+
+    using BaseInner::operator();
+
+    template <typename T>
+    requires Invokeable<F,T>()
+    void operator()(const T& t) const
+    {
+      f_(t);
+    }
+
+  private:
+    
+    F f_;
+  };
+
+  ComposeVariantVisitor(ArgsT&& args) : m_args(move(args))
+  {
+  }
+
+  template<typename Fadd>
+  auto add_handler(Fadd&& f)
+  {
+    return ComposeVariantVisitor<Fadd, Inner, pair<Fadd, ArgsT>>(
+        make_pair(move(f), move(m_args)));
+  }
+  
+  auto end_visitor()
+  {
+    return Inner(move(m_args));
+  }
+
+  ArgsT m_args;
+};
+
+struct EmptyVariantVisitor
+{
+  struct Inner
+  {                  
+    struct detail_t {};
+
+    Inner(nullptr_t)
+    {
+    }
+
+    void operator()(detail_t&) const {}
+  };
+
+  template <typename Fadd>
+  auto add_handler(Fadd&& f)
+  {
+    return ComposeVariantVisitor<Fadd, Inner, pair<Fadd, nullptr_t>>(
+        make_pair(move(f), nullptr));
+  }
+};
+
+EmptyVariantVisitor begin_variant_visitor()
+{
+    return EmptyVariantVisitor();
+}
+
+class A{};
+class B{};
+class C{};
+
+int main(int argc, char* argv[])
+{
+  
+  auto inline_printer = begin_variant_visitor()
+    .add_handler([](const A&) { cout << "A\n";})
+    .add_handler([](const B&) { cout << "B\n";})
+    .add_handler([](const C&) { cout << "C\n";})
+    .end_visitor();
+
+  A a;
+  inline_printer(a);
+  B b;
+  inline_printer(b);
+  C c;
+  inline_printer(c);
+  
+}
+
