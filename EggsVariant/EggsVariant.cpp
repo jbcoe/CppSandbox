854367a984a16aed1b6c58613e11444a0a06eb67
Jonathan Coe <jon@jbcoe.net>
2015-08-14 14:57:09 +0100

    apply method allows use of visitor

--- EggsVariant/EggsVariant.cpp
+++ EggsVariant/EggsVariant.cpp
@@ -14,10 +14,32 @@ class value : eggs::variant<std::string, double, int, bool, array, dict>
 {
   using variant_t = eggs::variant<std::string, double, int, bool, array, dict>;
   using variant_t::variant;
+
+public:
+
+  template <typename T, typename U=void>
+  U apply(T&& t) 
+  {
+    return eggs::variants::apply<U>(std::forward<T>(t), static_cast<variant_t&>(*this)); 
+  }
+};
+
+auto& os = std::cout;
+
+struct JSONPrinter
+{
+  void operator()(const std::string& s) { os << s; }
+  void operator()(double d) { os << d; }
+  void operator()(int i) { os << i; }
+  void operator()(bool b) { os << b; }
+  template <typename T>
+  void operator()(const T&) {}
 };
 
 int main(int argc, char* argv[])
 {
   value v("testing some JSON-handling"s);
+
+  v.apply(JSONPrinter());
 }
 
