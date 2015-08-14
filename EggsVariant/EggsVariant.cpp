cd2672fa3d01f0510c4e7a807f91aaaa731260d4
Jonathan Coe <jon@jbcoe.net>
2015-08-14 15:36:16 +0100

    Prints JSON (with comma issues)

--- EggsVariant/EggsVariant.cpp
+++ EggsVariant/EggsVariant.cpp
@@ -18,28 +18,86 @@ class value : eggs::variant<std::string, double, int, bool, array, dict>
 public:
 
   template <typename T, typename U=void>
-  U apply(T&& t) 
+  U apply(T&& t) const 
   {
-    return eggs::variants::apply<U>(std::forward<T>(t), static_cast<variant_t&>(*this)); 
+    return eggs::variants::apply<U>(std::forward<T>(t), static_cast<const variant_t&>(*this)); 
   }
+};
+
+auto indent = ""s;
+
+struct Indenter
+{
+  Indenter() { indent.push_back(' ');}
+  ~Indenter() { indent.pop_back();}
 };
 
 auto& os = std::cout;
 
 struct JSONPrinter
 {
-  void operator()(const std::string& s) { os << s; }
-  void operator()(double d) { os << d; }
-  void operator()(int i) { os << i; }
-  void operator()(bool b) { os << b; }
-  template <typename T>
-  void operator()(const T&) {}
+  void operator()(const std::string& s)
+  {
+    os << s;
+  }
+  
+  void operator()(double d)
+  {
+    os << d;
+  }
+  
+  void operator()(int i)
+  {
+    os << i;
+  }
+  
+  void operator()(bool b)
+  {
+    os << b;
+
+  }
+
+  void operator()(const array& a)
+  {
+    os << "[\n";
+    {        
+      Indenter in;
+      for (const auto& v : a)
+      {
+        os << indent;
+        v.apply(*this);
+        os << ",\n";
+      }
+    }
+    os << indent << "]";
+  }
+  
+  void operator()(const dict& d)
+  {
+    os << "{\n";
+    {
+      Indenter in;
+      for (const auto& kv : d)
+      {
+        os << indent << kv.first << ':';
+        kv.second.apply(*this);
+        os << ",\n";
+      }
+    }
+    os << indent << "}";
+  }
 };
 
 int main(int argc, char* argv[])
 {
   value v("testing some JSON-handling"s);
-
-  v.apply(JSONPrinter());
+  value av(std::vector<value>(3,v));
+  std::map<std::string,value> m;
+  m["Data"] = av;
+  m["Number"] = 1;
+  value mv=m;
+  value mav =(std::vector<value>(2,mv)); 
+  mav.apply(JSONPrinter());
+  std::cout << '\n';
 }
 
