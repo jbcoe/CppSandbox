e9f2e37ea7d2e8384f72ecddb150b4c7a866a18d
Jonathan Coe <jon@jbcoe.net>
2015-08-14 11:54:07 +0100

    JSON represented with eggs::variant

---
+++ EggsVariant/EggsVariant.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <eggs/variant.hpp>
+#include <vector>
+#include <map>
+#include <string>
+
+using namespace std::literals::string_literals;
+
+class value;
+using array = std::vector<value>;
+using dict = std::map<std::string, value>;
+
+class value : public eggs::variant<std::string, double, int, bool, array, dict>
+{
+  using variant_t = eggs::variant<std::string, double, int, bool, array, dict>;
+  using variant_t::variant;
+};
+
+int main(int argc, char* argv[])
+{
+  value v("testing some JSON-handling"s);
+}
+
