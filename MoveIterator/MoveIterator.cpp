06b979477e0bcd66f3e9c0b909c61e0595678e42
Jonathan Coe <jon@jbcoe.net>
2015-09-13 11:46:43 +0100

    add missing namespace std

--- MoveIterator/MoveIterator.cpp
+++ MoveIterator/MoveIterator.cpp
@@ -2,16 +2,14 @@
 #include <iterator>
 #include <vector>
 
-using namespace std;
-
 int main(int argc, char* argv[])
 {
-  vector<string> ingredients = {"slugs", "snails", "puppy-dogs' tails"};
-  vector<string> ingredients2;
-  copy(make_move_iterator(ingredients.begin()),
-       make_move_iterator(ingredients.end()), back_inserter(ingredients2));
+  std::vector<std::string> ingredients = {"slugs", "snails", "puppy-dogs' tails"};
+  std::vector<std::string> ingredients2;
+  std::copy(std::make_move_iterator(ingredients.begin()),
+       std::make_move_iterator(ingredients.end()), std::back_inserter(ingredients2));
 
-  ostream_iterator<string> os(std::cout, " ");
+  std::ostream_iterator<std::string> os(std::cout, " ");
 
   std::copy(ingredients.begin(), ingredients.end(), os);
   std::cout << std::endl;
