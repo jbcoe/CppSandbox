e030186134b90970eb0ef1f5d4518d3c12161923
Jonathan Coe <jon@jbcoe.net>
2016-02-08 22:41:04 +0000

    bind with reference argument

--- BindExperiments/BindExperiments.cpp
+++ BindExperiments/BindExperiments.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include <functional>
 
-int add(int x, int y) { return x+y; }
+int add(int x, int y)
+{
+  return x + y;
+}
+
+void accumulate(int value, int& sum)
+{
+  sum += value;
+}
 
 using namespace std::placeholders;
 
 int main(int argc, char* argv[])
 {
-  auto g = std::bind(add, _2, _1);
-  std::cout << g(40,2) << '\n';
+  auto f = std::bind(add,_1,_2);
+  std::cout << f(40,2) << '\n';
+  
+  auto g = std::bind(accumulate,_1,_2);
+  int x = 0;
+  g(40,x);
+  std::cout << x << '\n';
 }
 
