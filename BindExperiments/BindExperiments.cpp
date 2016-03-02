23c950c2a85062e867553fc091745eaaceebea4c
Jonathan Coe <jon@jbcoe.net>
2016-03-02 14:50:19 -0500

    bring submodules up to date

--- BindExperiments/BindExperiments.cpp
+++ BindExperiments/BindExperiments.cpp
@@ -15,10 +15,10 @@ using namespace std::placeholders;
 
 int main(int argc, char* argv[])
 {
-  auto f = std::bind(add,_1,_2);
+  auto f = [](auto && arg1, auto && arg2) { return add(arg1, arg2); };
   std::cout << f(40,2) << '\n';
   
-  auto g = std::bind(accumulate,_1,_2);
+  auto g = [](auto && arg1, auto && arg2) { return accumulate(arg1, arg2); };
   int x = 0;
   g(40,x);
   std::cout << x << '\n';
