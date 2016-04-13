7e1ef75e02e022271d147860f34dc6435e6ff97c
Jonathan Coe <jon@jbcoe.net>
2016-04-13 20:16:44 +0100

    add catch tests for rng covering std::forward use

--- AlgorithmWrappers/AlgorithmWrappers.cpp
+++ AlgorithmWrappers/AlgorithmWrappers.cpp
@@ -1,3 +1,6 @@
+#define CATCH_CONFIG_MAIN
+
+#include <catch.hpp>
 #include <algorithm>
 #include <numeric>
 #include <cmath>
@@ -7,7 +10,7 @@
 namespace rng
 {
   template <typename R, typename F>
-  bool any_of(const R&& r, F&& f)
+  bool any_of(const R& r, F&& f)
   {
     using std::begin;
     using std::end;
@@ -15,7 +18,7 @@ namespace rng
   }
   
   template <typename R, typename F>
-  bool all_of(const R&& r, F&& f)
+  bool all_of(const R& r, F&& f)
   {
     using std::begin;
     using std::end;
@@ -23,7 +26,7 @@ namespace rng
   }
   
   template <typename R, typename F>
-  bool none_of(const R&& r, F&& f)
+  bool none_of(const R& r, F&& f)
   {
     using std::begin;
     using std::end;
@@ -31,7 +34,7 @@ namespace rng
   }
   
   template <typename R, typename F>
-  size_t count_if(const R&& r, F&& f)
+  size_t count_if(const R& r, F&& f)
   {
     using std::begin;
     using std::end;
@@ -59,12 +62,63 @@ namespace rng
   }
 } // end namespace rng
 
-int main(int argc, char* argv[])
+template <typename F>
+class move_only_t
 {
-  std::vector<int> xs = {1,2,3,4,5,6,7,8,9,10};
-  auto pow = rng::map(xs, [](auto x) { return std::pow(2, x); });
-  auto q = rng::reduce(pow, std::plus<>());
-  std::cout << q << '\n';
-  return 0;
+  F f_;
+
+public:
+  template <typename... Ts>
+  auto operator()(Ts&&... ts) const
+  {
+    return f_(std::forward<Ts>(ts)...);
+  }
+
+  move_only_t(F&& f) : f_(std::move(f)) {}
+
+  move_only_t(move_only_t&&) = default;
+  move_only_t(const move_only_t&) = delete;
+};
+
+template <typename F>
+auto move_only(F&& f)
+{
+  return move_only_t<F>(std::move(f));
 }
+
+TEST_CASE("vector of ints with lambdas")
+{
+  const std::vector<int> data = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+  REQUIRE(rng::all_of(data, [](auto x) { return x < 10; }));
+  
+  REQUIRE(rng::any_of(data, [](auto x) { return x > 6; }));
+  
+  REQUIRE(rng::none_of(data, [](auto x) { return x > 10; }));
+  
+  REQUIRE(rng::count_if(data, [](auto x) { return x % 2 == 0; }) == 5);
+
+  REQUIRE(rng::reduce(data, [](auto sum, auto x) { return sum + x; }) == 45);
+
+  REQUIRE(rng::map(data, [](auto x) { return std::pow(2, x); })[9] == 512);
+}
+
+TEST_CASE("vector of ints with move_only wrapped lambdas")
+{
+  const std::vector<int> data = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+  REQUIRE(rng::all_of(data, move_only([](auto x) { return x < 10; })));
+
+  REQUIRE(rng::any_of(data, move_only([](auto x) { return x > 6; })));
+
+  REQUIRE(rng::none_of(data, move_only([](auto x) { return x > 10; })));
+
+  REQUIRE(rng::count_if(data, move_only([](auto x) { return x % 2 == 0; })) == 5);
+
+  REQUIRE(rng::reduce( data, move_only([](auto sum, auto x) { return sum + x; })) == 45);
+
+  REQUIRE(rng::map(data, move_only([](auto x) { return std::pow(2, x); }))[9] == 512);
+
+}
+
 
