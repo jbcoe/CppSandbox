b36e3c833b4b58110c1dab4dfe9a1bcefe17b638
Jonathan Coe <jon@jbcoe.net>
2016-04-13 09:21:58 +0100

    algorithm wrappers

--- AlgorithmWrappers/AlgorithmWrappers.cpp
+++ AlgorithmWrappers/AlgorithmWrappers.cpp
@@ -9,33 +9,43 @@ namespace rng
   template <typename R, typename F>
   bool any_of(const R&& r, F&& f)
   {
-    return std::any_of(r.begin(), r.end(), std::forward<F>(f));
+    using std::begin;
+    using std::end;
+    return std::any_of(begin(r), end(r), std::forward<F>(f));
   }
   
   template <typename R, typename F>
   bool all_of(const R&& r, F&& f)
   {
-    return std::all_of(r.begin(), r.end(), std::forward<F>(f));
+    using std::begin;
+    using std::end;
+    return std::all_of(begin(r), end(r), std::forward<F>(f));
   }
   
   template <typename R, typename F>
   bool none_of(const R&& r, F&& f)
   {
-    return std::none_of(r.begin(), r.end(), std::forward<F>(f));
+    using std::begin;
+    using std::end;
+    return std::none_of(begin(r), end(r), std::forward<F>(f));
   }
   
   template <typename R, typename F>
   size_t count_if(const R&& r, F&& f)
   {
-    return std::count_if(r.begin(), r.end(), std::forward<F>(f));
+    using std::begin;
+    using std::end;
+    return std::count_if(begin(r), end(r), std::forward<F>(f));
   }
 
   template <typename R, typename F>
   auto map(const R& r, F&& f)
   {
+    using std::begin;
+    using std::end;
     using result_type = decltype(f(r.front()));
     std::vector<result_type> results(r.size());
-    std::transform(r.begin(), r.end(), results.begin(), std::forward<F>(f));
+    std::transform(begin(r), end(r), results.begin(), std::forward<F>(f));
     return results;
   }
 
@@ -43,7 +53,9 @@ namespace rng
             typename V = decltype(*std::declval<const R&>().begin())>
   auto reduce(const R& r, F&& f, V v = V{})
   {
-    return std::accumulate(r.begin(), r.end(), std::forward<F>(f), v);
+    using std::begin;
+    using std::end;
+    return std::accumulate(begin(r), end(r), std::move(v), std::forward<F>(f));
   }
 } // end namespace rng
 
@@ -52,6 +64,7 @@ int main(int argc, char* argv[])
   std::vector<int> xs = {1,2,3,4,5,6,7,8,9,10};
   auto pow = rng::map(xs, [](auto x) { return std::pow(2, x); });
   auto q = rng::reduce(pow, std::plus<>());
+  std::cout << q << '\n';
   return 0;
 }
 
