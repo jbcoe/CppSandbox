1cd4e0c8a0b6a397b4df625461828851aedb211c
Jonathan B. Coe <jbcoe@me.com>
2016-12-01 14:31:52 +0000

    neatening up with arg_t

--- PassByValue/PassByValue.cpp
+++ PassByValue/PassByValue.cpp
@@ -3,49 +3,37 @@
 #include <type_traits>
 
 template <typename T>
-class array_view
-{
+class array_view {
   size_t size_ = 0;
   const T* data_ = nullptr;
 
 public:
   array_view(const std::initializer_list<T>& l)
-      : size_(l.size()), data_(l.begin())
-  {
-  }
+      : size_(l.size()), data_(l.begin()) {}
 
-  const T* data() const
-  {
-    return data_;
-  }
-
-  size_t size() const
-  {
-    return size_;
-  }
-
-  const T* begin() const
-  {
-    return data_;
-  }
-
-  const T* end() const
-  {
-    return data_ + size_;
-  }
+  const T* data() const { return data_; }
+  size_t size() const { return size_; }
+  const T* begin() const { return data_; }
+  const T* end() const { return data_ + size_; }
 };
-
-#if defined(PASS_BY_VALUE)
-void print_range(array_view<int> r);
-#elif defined(PASS_BY_REF)
-void print_range(const array_view<int>& r);
-#else
-#error Define PASS_BY_VALUE or PASS_BY_REF
+#if !defined(VALUE_SIZE)
+#define VALUE_SIZE 32
 #endif
+template <typename T, bool=(sizeof(T)<=VALUE_SIZE)> struct arg;
 
-int main(int argc, char* argv[])
-{
-  array_view<int> ints = {0, 1, 2, 3, 4, 5, 6};
+template <typename T>
+struct arg<T, false> { using type = const T&; };
+  
+template <typename T>
+struct arg<T, true> { using type = T; };
+
+template <typename T>
+using arg_t = typename arg<T>::type;
+
+void print_range(arg_t<array_view<int>>);               
+
+int main() {
+  auto ints = {0, 1, 2, 3, 4, 5, 6};
   print_range(ints);
+  return 0;
 }
-
