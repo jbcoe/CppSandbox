5134012765479ec3d226b91ac930d4b1eeddee9b
Jonathan B. Coe <jbcoe@me.com>
2017-07-28 19:48:31 +0100

    good and bad behaviour can be controlled with a DEFINE

--- ASANAlignment/ASANAlignment.cpp
+++ ASANAlignment/ASANAlignment.cpp
@@ -1,23 +1,37 @@
-#include <iostream>
+#include <cstring>
 
-int sum(const char* buffer)
-{
-  const int* values = (const int*)(buffer);
-  
-  return values[0] + values[1];
+//#define SAFE
+
+int sum(const char* buffer) {
+  int x;
+  int y;
+#ifndef SAFE
+  x = *(int*)buffer;
+  y = *(int*)(buffer + sizeof(int));
+#else
+  std::memcpy(&x, buffer, sizeof(int));
+  std::memcpy(&y, buffer + sizeof(int), sizeof(int));
+#endif
+  return x + y;
 }
 
-int main(int argc, char* argv[])
-{
-  auto buffer = new char[3*sizeof(int)];
- 
-  int* xs = (int*)&buffer[3];
-  xs[0] = 2;
-  xs[1] = 5;
+int main(int argc, char* argv[]) {
+  auto buffer = new char[3 * sizeof(int)];
 
-  std::cout << sum(&buffer[3]) << std::endl;
-  
-  delete [] buffer;
+  int x = 2;
+  int y = 5;
+
+#ifndef SAFE
+  *(int*)(buffer + 4) = x;
+  *(int*)(buffer + 4 + sizeof(int)) = y;
+#else
+  std::memcpy(buffer + 4, &x, sizeof(int));
+  std::memcpy(buffer + 4 + sizeof(int), &y, sizeof(int));
+#endif
+
+  auto r = sum(&buffer[4]);
+
+  delete[] buffer;
+
+  return r - 7;
 }
-
-
