a77e3011fff268dc96a2b12bb33296efbd311b98
Jonathan B. Coe <jonathanbcoe@gmail.com>
2017-10-16 15:06:55 +0100

    improved ranges demo

--- RangesV3Demo_View/RangesV3Demo_View.cpp
+++ RangesV3Demo_View/RangesV3Demo_View.cpp
@@ -6,10 +6,13 @@ using namespace ranges;
 
 int main(int argc, char* argv[])
 {
-  for_each(view::ints(1) | view::take(10) 
-                         | view::remove_if([](auto x) { return x % 2 == 0; }) 
-                         | view::transform([](auto x) { return x * x; }),
-           [](auto x) { std::cout << x << ' '; });
+  for( auto x : view::ints(1) 
+              | view::remove_if([](auto x) { return x % 2 == 0; }) 
+              | view::transform([](auto x) { return x * x; })
+              | view::take(10))
+  {
+    std::cout << x << ' ';
+  }
 
   std::cout << std::endl;
 }
--- compilerSettings_clang-latest.mk
+++ compilerSettings_clang-latest.mk
@@ -1,5 +1,5 @@
-CC=/opt/llvm/bin/clang
-CXX=/opt/llvm/bin/clang++
+CC=/usr/local/Cellar/llvm/5.0.0/bin/clang
+CXX=/usr/local/Cellar/llvm/5.0.0/bin/clang++
 RM=rm -f
 OPT=0
 CPPFLAGS= -Wall -Winline -Wwrite-strings -Wno-unused -O$(OPT) -std=c++14 -stdlib=libc++ -I../
